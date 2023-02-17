a0f39831bdbe1e4508a54586aef84d3d265c5130
Hambaiki <92420824+Hambaiki@users.noreply.github.com>
2023-02-17 11:58:42 +0700

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -6,112 +6,111 @@ const Signup = () => {
   return (
     <>
       <div className="m-auto">
-        <div className="flex bg-white rounded shadow-lg">
+        <div className="flex bg-white rounded shadow-lg max-w-3xl">
           <div className="flex flex-col m-6">
-            <p className="text-center mb-4 text-3xl font-bold text-slate-600">
+            <p className="text-center mb-2 text-3xl font-bold text-slate-600">
               Create Account
             </p>
-            <form className="mx-8">
-              <div className="flex flex-row justify-between">
-                <lable>
-                  <p className="text-gray-600 text-sm">
-                    First Name
-                  </p>
-                  <input
-                    type="username"
-                    className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="InputEmail"
-                    placeholder="Username"
-                  />
-                </lable>
-                <lable>
-                  <p className="text-gray-600 text-sm">
-                    Last Name
-                  </p>
-                  <input
-                    type="username"
-                    className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="InputEmail"
-                    placeholder="Username"
-                  />
-                </lable>
-              </div>
-              <div className="flex flex-col">
-                <lable>
-                  <p className="text-gray-600 text-sm">
-                    Username
-                  </p>
-                  <input
-                    type="username"
-                    className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="InputEmail"
-                    placeholder="Username"
-                  />
-                </lable>
-              </div>
-              <div className="flex flex-row justify-between">
-                <lable>
-                  <p className="text-gray-600 text-sm">
-                    First Name
-                  </p>
-                  <input
-                    type="username"
-                    className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="InputEmail"
-                    placeholder="Username"
-                  />
-                </lable>
-                <lable>
-                  <p className="text-gray-600 text-sm">
-                    Last Name
-                  </p>
-                  <input
-                    type="username"
-                    className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="InputEmail"
-                    placeholder="Username"
-                  />
-                </lable>
-              </div>
-              <div className="flex justify-between mb-4">
-                <div>
-                  <label>
+            <form>
+              <div className="flex flex-col space-y-2 mb-4">
+                <div className="flex flex-row justify-between space-x-4">
+                  <lable>
+                    <p className="text-gray-600 text-xs mb-1">
+                      First Name
+                    </p>
                     <input
-                      type="checkbox"
-                      className="w-4 h-4S bg-gray-100 border-gray-300 rounded"
-                      id="CheckRememberMe"
+                      type="firstname"
+                      className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      id="InputFirstName"
+                      placeholder="First Name"
                     />
-                    <a className="ml-2 text-xs font-medium text-gray-400">
-                      Remember Me
-                    </a>
-                  </label>
+                  </lable>
+                  <lable>
+                    <p className="text-gray-600 text-xs mb-1">
+                      Last Name
+                    </p>
+                    <input
+                      type="lastname"
+                      className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      id="InputLastName"
+                      placeholder="Last Name"
+                    />
+                  </lable>
+                </div>
+                <div className="flex flex-col">
+                  <lable>
+                    <p className="text-gray-600 text-xs mb-1">
+                      Username
+                    </p>
+                    <input
+                      type="username"
+                      className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      id="InputUsername"
+                      placeholder="Username"
+                    />
+                  </lable>
+                </div>
+                <div className="flex flex-row justify-between space-x-4">
+                  <lable>
+                    <p className="text-gray-600 text-xs mb-1">
+                      Password
+                    </p>
+                    <input
+                      type="password"
+                      className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      id="InputPassword"
+                      placeholder="********"
+                    />
+                  </lable>
+                  <lable>
+                    <p className="text-gray-600 text-xs mb-1">
+                      Confirm Password
+                    </p>
+                    <input
+                      type="passwordConfirm"
+                      className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      id="InputPasswordConfirm"
+                      placeholder="********"
+                    />
+                  </lable>
                 </div>
               </div>
-              <div className="flex items-center justify-center">
-                <button type="submit" className="bg-blue-900 text-white shadow font-bold py-2 px-16 rounded-lg focus:outline-none focus:shadow-outline">
-                  Log In
+              <div className="flex flex-col space-y-2">
+                <label>
+                  <input
+                    type="checkbox"
+                    className="w-4 h-4S bg-gray-100 border-gray-300 rounded"
+                    id="CheckAgree"
+                  />
+                  <a className="ml-2 text-xs font-medium text-gray-400">
+                    I have agreed to <a href="#" className="font-bold"> ayayayayay </a>
+                  </a>
+                </label>
+                <button type="submit" className="bg-blue-900 text-white shadow font-bold py-2 grow rounded-3xl focus:outline-none focus:shadow-outline">
+                  Sign Up
                 </button>
               </div>
+
             </form>
             <div className="flex justify-center m-4">
               <p className="text-xs font-light text-gray-400">
-                ---------------- OR ----------------
+                --------- OR sign up with ---------
               </p>
             </div>
-            <div className="flex flex-col mb-2 mx-8">
-              <div className="flex items-center justify-center mb-4">
-                <button className="bg-transparent grow text-gray-600 border border-grey-500 py-2 font-semilight rounded-lg text-sm" type="button">
-                  Continue with Google
+            <div className="flex justify-between mb-2 mx-8 space-x-2">
+              <div className="flex flex-auto">
+                <button className="bg-transparent grow text-gray-600 border border-grey-500 py-2 font-semilight rounded-3xl text-sm" type="button">
+                  Google
                 </button>
               </div>
-              <div className="flex items-center justify-center mb-4">
-                <button className="bg-transparent grow text-gray-600 border border-grey-500 py-2 font-semilight rounded-lg text-sm" type="button">
-                  Continue with Facebook
+              <div className="flex flex-auto">
+                <button className="bg-transparent grow text-gray-600 border border-grey-500 py-2 font-semilight rounded-3xl text-sm" type="button">
+                  Facebook
                 </button>
               </div>
-              <div className="flex items-center justify-center mb-4">
-                <button className="bg-transparent grow text-gray-600 border border-grey-500 py-2 font-semilight rounded-lg text-sm" type="button">
-                  Continue with Email
+              <div className="flex flex-auto">
+                <button className="bg-transparent grow text-gray-600 border border-grey-500 py-2 font-semilight rounded-3xl text-sm" type="button">
+                  Email
                 </button>
               </div>
             </div>
