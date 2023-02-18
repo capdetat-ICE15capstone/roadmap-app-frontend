3993864c085e8ab4a57352b42871c9a962e51a7a
SupakornSJB <limit4break@gmail.com>
2023-02-18 15:18:22 +0700

    - docs: write some comments as a helper in roadmapcreatepage.jsx - chores: seperate modal code in roadmapcreatepage into a seperate file

---
+++ src/components/TaskModal.jsx
@@ -0,0 +1,66 @@
+import React, { useRef } from "react";
+import PropTypes from 'prop-types'
+
+const TaskModal = ({ mode, oldData, editTaskCallBack }) => {
+    const nameRef = useRef("");
+    const descriptionRef = useRef("");
+
+    const placeHolderFunction = (someText="world") => {
+    console.log(`Hello, ${someText}`)
+    }
+  
+    return (
+        <>
+            <div
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+          >
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-2 border-emerald-600 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">
+                    Create task
+                  </h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => editTaskCallBack("failed", {})}
+                  > x
+                  </button>
+                </div>
+                {/*body*/}
+                <div>
+                    Hello world
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => editTaskCallBack("failed", [])}
+                  >
+                    Close
+                  </button>
+                  <button
+                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => editTaskCallBack("failed", [])}
+                  >
+                    Save Changes
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+    )
+  }
+
+  TaskModal.propTypes = {
+    mode: PropTypes.string.isRequired,
+    oldData: PropTypes.object,
+    editTaskCallBack: PropTypes.func
+  }
+
+  export default TaskModal;
--- src/pages/RoadmapCreatePage.jsx
+++ src/pages/RoadmapCreatePage.jsx
@@ -1,92 +1,21 @@
 import React, { useRef, useState, useEffect } from "react";
-
-const Modal = ({addTaskCallback, setShowModal, id, setId}) => {
-  const nameRef = useRef("");
-  const descriptionRef = useRef("");
-
-  const clickHandler = (isCreate) => {
-      if(isCreate) {
-          addTaskCallback({
-              "id": id,
-              "name": nameRef.current.value,
-              "description": descriptionRef.current.value
-          })
-          setId(id+1);
-      } 
-      setShowModal(false);
-  }
-
-  return (
-      <>
-          <div
-          className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-        >
-          <div className="relative w-auto my-6 mx-auto max-w-3xl">
-            {/*content*/}
-            <div className="border-2 border-emerald-600 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-              {/*header*/}
-              <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                <h3 className="text-3xl font-semibold">
-                  Create task
-                </h3>
-                <button
-                  className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                  onClick={() => setShowModal(false)}
-                > x
-                </button>
-              </div>
-              {/*body*/}
-              <div className="relative p-6 flex flex-col space-y-1">
-                <div className="flex justify-between ">
-                  <span >Name:</span> 
-                  <input type="text" ref={nameRef} className="border-emerald-500 border-2 rounded-md"></input>
-                </div>
-                <div className="flex space-x-3">
-                  <span className="flex">Description:</span> 
-                  <input type="text" ref={descriptionRef} className="border-emerald-500 border-2 rounded-md"></input>
-                </div>
-                  
-              </div>
-              {/*footer*/}
-              <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-                <button
-                  className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                  type="button"
-                  onClick={() => clickHandler(false)}
-                >
-                  Close
-                </button>
-                <button
-                  className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                  type="button"
-                  onClick={() => clickHandler(true)}
-                >
-                  Save Changes
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-      </>
-  )
-}
+import TaskModal  from "../components/TaskModal";
 
 const RoadmapCreatePage = () => {
   const [RMName, setRMName] = useState("");
   const [RMDesc, setRMDesc] = useState("")
   const [tasks, setTasks] = useState([])
   const [modalState, setModalState] = useState(false)
-  const [id, setId] = useState(0) // might be later fetched
-  // {taskid, taskname, taskstatus}
 
   useEffect(() => {
     console.log(tasks)
   }, [tasks])
   
-  const addTaskClickhandler = (submissionObject) => {
-    setTasks([...tasks, {
-      id: submissionObject.id,
+  const editTaskCallBack = (success, submissionObject) => {
+    // need more logic as describe in the sheet
+
+    setModalState(false);
+    setTasks([...tasks, { // these 4 lines can be used for adding task, not edit
       name: submissionObject.name,
       description: submissionObject.description
     }])
@@ -94,7 +23,7 @@ const RoadmapCreatePage = () => {
 
   const onPressingSaveButton = (e) => {
     e.preventDefault(); // stop the page from reloading when submitting the form, may remove in the future
-    console.log({
+    console.log({ // used for sprint 1
       name: RMName,
       description: RMDesc
     });
@@ -148,8 +77,9 @@ const RoadmapCreatePage = () => {
           <button className="bg-emerald-500 hover:bg-yellow-500 p-2 m-2 h-10 w-16 self-center rounded-md text-white font-bold" onClick={() => setModalState(true)}>Add</button>
         </div>
         {
-          modalState ? <Modal setShowModal={(e) => setModalState(e)} addTaskCallback={(e) => addTaskClickhandler(e)}
-            id={id} setId={setId}/> : null
+          // currently the mode and olddata attr is hard coded
+          // both should be changed to correspond to the correct node type (new node or edit old node)
+          modalState ? <TaskModal mode="create" oldData={{}} editTaskCallBack={editTaskCallBack}/> : null
         }
       </div>
     </>
