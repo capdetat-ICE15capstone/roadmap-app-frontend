20753a4fbaddb467c54107933011248c48b93800
ThanapatTr <6338098421@student.chula.ac.th>
2023-03-18 00:13:11 +0700

    RoadmapCreate navigate to createRoadMap page

--- src/components/RoadmapCreate.jsx
+++ src/components/RoadmapCreate.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { ReactComponent as PlusIcon } from "../assets/roadmap_assets/plus_Icon.svg"
+import { Link } from "react-router-dom";
+
 
 
 const RoadmapCreate = () => {
     return (
-        < div className="relative flex justify-center items-center border-1 border-dashed border-black border-2 w-80 h-80 m-8" >
-            {/*<PlusIcon className="absolute h-full w-full" />*/}
-            <PlusIcon className="absolute "/>
-        </ div>
-    )
+        <Link to={"/createRoadMap"} className="relative flex items-center justify-center w-80 h-80 m-8 border-dashed border-2 border-gray-400 rounded-[48px]">
+            <PlusIcon className="w-16 h-16" />
+        </Link>
+    );
 };
-
 export default RoadmapCreate;
