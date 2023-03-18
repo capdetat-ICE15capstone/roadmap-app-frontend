b914523887f523ba08410672a28c26f4bcac450d
wuttikorn.n45 <wuttikorn.n45@gmail.com>
2023-03-18 17:58:42 +0700

    change color of searchButton

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const SearchButton = () => {
     return (
-        <button type="submit"className="bg-[#DF5E76] hover:bg-[#BE4057] text-white font-bold appearance-none border rounded-3xl px-12 py-4 ml-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+        <button type="submit"className="bg-[#00286E] hover:bg-[#011C4B] text-white font-bold appearance-none border rounded-3xl px-12 py-4 ml-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
             Search
         </button>
       )
