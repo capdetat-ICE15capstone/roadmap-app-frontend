055a3e09efa17ea68ebf5552769980a58b63b1c0
wuttikorn.n45 <wuttikorn.n45@gmail.com>
2023-03-18 13:19:08 +0700

    modify RoadmapCreate position

--- src/components/RoadmapCreate.jsx
+++ src/components/RoadmapCreate.jsx
@@ -2,12 +2,10 @@ import React from "react";
 import { ReactComponent as PlusIcon } from "../assets/roadmap_assets/plus_Icon.svg"
 import { Link } from "react-router-dom";
 
-
-
 const RoadmapCreate = () => {
     return (
         <Link to={"/createRoadMap"} className="relative flex items-center justify-center w-80 h-80 m-8 border-dashed border-2 border-gray-400 rounded-[48px]">
-            <PlusIcon className="w-16 h-16" />
+            <PlusIcon className="absolute w-16 h-16" />
         </Link>
     );
 };
