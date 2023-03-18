c7202e02d475940b1951b5e99f245c8ee8d44d0f
wuttikorn.n45 <wuttikorn.n45@gmail.com>
2023-03-18 22:36:05 +0700

    just adjust indents

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -8,12 +8,12 @@ const Feed = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("search:" + search);
+    console.log("search: " + document.getElementById("roadmapName").value);
   };
-    return (
-      <>
-      {/*Search bar*/}
-      <form className="mx-auto" id="searchForm" onSubmit={handleSubmit}>
+  return (
+    <>
+    {/*Search bar*/}
+    <form className="mx-auto" id="searchForm" onSubmit={handleSubmit}>
       <div
         className="relative inline-block my-4"
         onSubmit={handleSubmit}
@@ -22,8 +22,37 @@ const Feed = () => {
         <SearchButton/>
       </div>
     </form>
-      </>
-    )
+    <div className="relative inline-block mx-4">
+      <div className="inline-block">
+        <Roadmap
+        userName="BomberMan"
+        ownerName="Unknown" 
+        roadmapName="Commiting Suicide" 
+        createdDate="10/26/2022" 
+        updateDate="02/16/2023" 
+        views={69} />
+      </div>
+      <div className="inline-block">
+        <Roadmap 
+        userName="OctopusLarry"
+        ownerName="Fing" 
+        roadmapName="Downloading Minecraft" 
+        createdDate="04/26/2020" 
+        updateDate="07/16/2021" 
+        views={420} />
+      </div>
+      <div className="inline-block">
+        <Roadmap
+        userName="Lonely_Fing" 
+        ownerName="Nantawitaya" 
+        roadmapName="Playing Paladins" 
+        createdDate="05/02/2018" 
+        updateDate="03/18/2023" 
+        views={177013} />
+      </div>
+    </div>
+    </>
+  )
 }
 
 export default Feed;
