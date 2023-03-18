47b1435b887ce06f15cdd92927a2455cc9d2eb35
Hambaiki <92420824+Hambaiki@users.noreply.github.com>
2023-03-18 20:11:31 +0700

    Navbar update
    Remove top bar to fit with the current layout. Modified collapsing behavior.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,24 +28,19 @@ const Navbar = () => {
   return (
     <>
       <div className="flex fixed h-screen w-screen">
-        <div className="flex flex-col h-screen bg-nav-black justify-between w-0 md:w-36">
-          <div className="flex flex-col">
-            <Logo className="justify-self-center self-center h-20" />
-            <NavItem SvgIcon={HomeLogo} displayName="Home" to="/" />
-            <NavItem SvgIcon={SettingLogo} displayName="Setting" to="/setting" />
-            <NavItem SvgIcon={CalendarLogo} displayName="Calendar" to="/calendar" />
+        <div className="md:visible collapse">
+          <div className="flex flex-col h-screen bg-nav-black justify-between w-36">
+            <div className="flex flex-col">
+              <Logo className="justify-self-center self-center h-20" />
+              <NavItem SvgIcon={HomeLogo} displayName="Home" to="/" />
+              <NavItem SvgIcon={SettingLogo} displayName="Setting" to="/setting" />
+              <NavItem SvgIcon={CalendarLogo} displayName="Calendar" to="/calendar" />
+            </div>
+            <NavItem SvgIcon={UserLogo} displayName="Username" to="/profile"
+              parentDivClass="bg-nav-gray py-7" />
           </div>
-          <NavItem SvgIcon={UserLogo} displayName="Username" to="/profile"
-            parentDivClass="bg-nav-gray py-7" />
         </div>
         <div className="flex flex-col grow">
-          <div className="sticky top-0 left-0 h-20 bg-nav-gray p-4 flex justify-end">
-            <div className="flex">
-              <BellIcon className="self-center w-10 px-2 hover:scale-110 transition duration-300" />
-              <BookIcon className="self-center px-2 w-10 hover:scale-110 transition duration-300" />
-            </div>
-            
-          </div>
           <Outlet />
         </div>
       </div>
