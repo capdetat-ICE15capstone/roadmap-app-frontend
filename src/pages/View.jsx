dfa63fbbf1dbfac1835714b92ed974cd15306572
Hambaiki <92420824+Hambaiki@users.noreply.github.com>
2023-03-19 14:15:19 +0700

    Update View.jsx
    Implement functionality to content viewer. Collapse and expand description box. Separate UI elements into components.

--- src/pages/View.jsx
+++ src/pages/View.jsx
@@ -1,173 +1,217 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
-import LineTo from 'react-lineto';
 
 function View() {
+  const [currentMilestone, setCurrentMilestone] = useState('1');
+  const [currentDetail, setCurrentDetail] = useState("");
+  const [detailToggle, setCurrentDetailToggle] = useState(true);
+
+  // useEffect(() => {
+  //   return () => {
+  //     console.log('test');
+  //       setCurrentDetail(
+
+  //       );
+  //   }
+  // }, [currentMilestone])
+
+  function MilestoneDetails(props) {
+    const current = parseInt(props.milestone);
+    return (
+      <div className='flex bg-white rounded-lg shadow-lg p-4'>
+        <div className='flex flex-col bg-white p-4 space-y-2 w-1/2'>
+          <div className='font-bold'>
+            {roadmap.tasks[current - 1].name}
+          </div>
+          <div className='text-sm'>
+            {roadmap.tasks[current - 1].description}
+          </div>
+          <div className='flex space-x-2'>
+            <div className='bg-green-100 grow font-bold text-center p-2'>
+              {roadmap.tasks[current - 1].startDate}
+            </div>
+            <div className='bg-red-100 grow font-bold text-center p-2'>
+              {roadmap.tasks[current - 1].dueDate}
+            </div>
+          </div>
+        </div>
+        <div className='flex flex-col bg-white p-4 space-y-2 w-1/2 shrink-0'>
+          <div className='font-bold'>
+            Sub-Tasks
+          </div>
+          <div className='flex flex-col justify-center space-y-2 text-sm'>
+            {roadmap.tasks.map(task => 
+              {
+
+              })
+            }
+            <label>
+              <input
+                type="checkbox"
+                className="w-4 h-4S mr-2 bg-gray-100 border-gray-300 rounded"
+                id="CheckRememberMe"
+                onChange={() => setRememberMe(!rememberMe)}
+              />
+              lacus eget faucibus tempor
+            </label>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  function RoadmapDetails(props) {
+    return (
+      <div className='flex flex-col bg-white rounded-lg shadow-lg p-4 space-y-4'>
+        <div className='flex justify-between'>
+          <div className='text-3xl font-extrabold'>
+            {roadmap.name}
+          </div>
+          <button onClick={() => setCurrentDetailToggle(!detailToggle)} className="bg-transparent text-gray-600 border border-grey-500 px-4 py-2 font-semilight rounded-lg text-sm" type="button">
+            Expand/Collapse
+          </button>
+        </div>
+        <div className={`${(detailToggle) ? 'visible' : 'hidden'}`}>
+          <div className='flex flex-col bg-slate-100 rounded p-4 text-sm'>
+            {roadmap.description}
+          </div>
+        </div>
+        <div className="flex space-x-4">
+          <button className="bg-transparent grow text-gray-600 border border-grey-500 px-4 py-2 font-semilight rounded-lg text-sm" type="button">
+            Modify
+          </button>
+          <button className="bg-transparent grow text-gray-600 border border-grey-500 px-4 py-2 font-semilight rounded-lg text-sm" type="button">
+            Clone
+          </button>
+          <button className="bg-transparent grow text-gray-600 border border-grey-500 px-4 py-2 font-semilight rounded-lg text-sm" type="button">
+            Something?
+          </button>
+        </div>
+      </div >
+    )
+  }
+
 
   const [roadmap, setRoadmap] = useState({
-    name: 'Dummy Roadmap',
-    description: 'none',
-    publicity: true,
-    tasks: [{
-      'id': '1',
-      'name': 'start',
-      'description': 'none',
-      'startDate': '20200110',
-      'dueDate': '20200115',
-      'nodeColor': 'green',
-      'nodeShape': 'circle',
-      'subtasks': [{
+    'name': 'Lorem Ipsum Roadmap',
+    'description': 'Morbi facilisis finibus lacus quis aliquam. Vestibulum turpis nibh, imperdiet non gravida quis, pretium vitae est. Phasellus in sollicitudin quam, id lacinia nisl.',
+    'publicity': true,
+    'tasks': [
+      {
         'id': '1',
-        'detail': 'none',
-        'status': false,
-      }]
-    }]
+        'name': 'start',
+        'description': 'Task descriptions. This is the task for the first milestone.',
+        'startDate': '20200110',
+        'dueDate': '20200115',
+        'nodeColor': 'green',
+        'nodeShape': 'circle',
+        'subtasks': [
+          {
+            'id': '1',
+            'detail': 'Morbi facilisis finibus lacus quis aliquam.',
+            'status': false,
+          },
+          {
+            'id': '2',
+            'detail': 'Vestibulum turpis nibh.',
+            'status': false,
+          }
+        ]
+      },
+      {
+        'id': '2',
+        'name': 'second',
+        'description': 'Task descriptions. This is the task for the second milestone.',
+        'startDate': '20200115',
+        'dueDate': '20200120',
+        'nodeColor': 'blue',
+        'nodeShape': 'square',
+        'subtasks': [
+          {
+            'id': '1',
+            'detail': 'Morbi facilisis finibus lacus quis aliquam.',
+            'status': false,
+          },
+          {
+            'id': '2',
+            'detail': 'Vestibulum turpis nibh.',
+            'status': false,
+          }
+        ]
+      },
+      {
+        'id': '3',
+        'name': 'third',
+        'description': 'Task descriptions. This is the task for the third milestone.',
+        'startDate': '20200120',
+        'dueDate': '20200121',
+        'nodeColor': 'yellow',
+        'nodeShape': 'triangle',
+        'subtasks': [
+          {
+            'id': '1',
+            'detail': 'Morbi facilisis finibus lacus quis aliquam.',
+            'status': false,
+          },
+          {
+            'id': '2',
+            'detail': 'Vestibulum turpis nibh.',
+            'status': false,
+          }
+        ]
+      }
+    ]
   });
 
-  const description = "Morbi facilisis finibus lacus quis aliquam. Vestibulum turpis nibh, imperdiet non gravida quis, pretium vitae est. Phasellus in sollicitudin quam, id lacinia nisl. Suspendisse non commodo est. Vestibulum ut mauris fermentum, laoreet ex sed, elementum ante. Sed mollis neque vel purus laoreet condimentum.";
+  // useEffect(() => {
+  //   //Runs on the first render
+  //   //And any time any dependency value changes
+  // }, [currentMilestone]);
 
-  console.log(roadmap);
+  const description = "Morbi facilisis finibus lacus quis aliquam. Vestibulum turpis nibh, imperdiet non gravida quis, pretium vitae est. Phasellus in sollicitudin quam, id lacinia nisl. Suspendisse non commodo est. Vestibulum ut mauris fermentum, laoreet ex sed, elementum ante. Sed mollis neque vel purus laoreet condimentum.";
 
   return (
     <div>
       <div className='flex h-screen bg-gray-200 overflow-y-scroll'>
         <div className="container w-5/6 flex-col m-auto py-8 space-y-4">
-          <div className='flex flex-col bg-white rounded-lg shadow-lg p-4 space-y-4'>
-            <div className='flex justify-between'>
-              <div className='text-3xl font-extrabold'>
-                Lorem Ipsum
-              </div>
-              <button className="bg-transparent text-gray-600 border border-grey-500 px-4 py-2 font-semilight rounded-lg text-sm" type="button">
-                Expand/Collapse
-              </button>
-            </div>
-            <div className='flex flex-col bg-slate-100 rounded p-4 text-sm'>
-              #lorem #ipsum
-            </div>
-            <div className='flex flex-col bg-slate-100 rounded p-4 text-sm'>
-              {description}
-            </div>
-            <div className="flex space-x-4">
-              <button className="bg-transparent grow text-gray-600 border border-grey-500 px-4 py-2 font-semilight rounded-lg text-sm" type="button">
-                Button A
-              </button>
-              <button className="bg-transparent grow text-gray-600 border border-grey-500 px-4 py-2 font-semilight rounded-lg text-sm" type="button">
-                Button B
-              </button>
-              <button className="bg-transparent grow text-gray-600 border border-grey-500 px-4 py-2 font-semilight rounded-lg text-sm" type="button">
-                Button C
-              </button>
-            </div>
-          </div>
+          <RoadmapDetails />
           <div className='flex bg-white rounded-lg shadow-lg p-4 overflow-x-auto'>
             <div className='flex m-8'>
-              <div className='flex items-center'>
-                <div className='p-4 rounded-full bg-slate-500 text-white'>
-                  01
-                </div>
-              </div>
-              <div className='flex items-center'>
-                <hr className="w-8 h-1 bg-gray-100 border-0 md:my-10 dark:bg-gray-700" />
-                <div className='p-4 rounded-full bg-slate-500 text-white'>
-                  02
-                </div>
-              </div>
-              <div className='flex items-center'>
-                <hr className="w-8 h-1 bg-gray-100 border-0 md:my-10 dark:bg-gray-700" />
-                <div className='p-4 rounded-full bg-slate-500 text-white'>
-                  03
-                </div>
-              </div>
-              <div className='flex items-center'>
-                <hr className="w-8 h-1 bg-gray-100 border-0 md:my-10 dark:bg-gray-700" />
-                <div className='p-4 rounded-full bg-slate-500 text-white'>
-                  04
-                </div>
-              </div>
-              <div className='flex items-center'>
-                <hr className="w-8 h-1 bg-gray-100 border-0 md:my-10 dark:bg-gray-700" />
-                <div className='p-4 rounded-full bg-slate-500 text-white'>
-                  05
-                </div>
-              </div>
-              <div className='flex items-center'>
-                <hr className="w-8 h-1 bg-gray-100 border-0 md:my-10 dark:bg-gray-700" />
-                <div className='p-4 rounded-full bg-slate-500 text-white'>
-                  06
-                </div>
-              </div>
-              <div className='flex items-center'>
-                <hr className="w-8 h-1 bg-gray-100 border-0 md:my-10 dark:bg-gray-700" />
-                <div className='p-4 rounded-full bg-slate-500 text-white'>
-                  07
-                </div>
-              </div>
-              <div className='flex items-center'>
-                <hr className="w-8 h-1 bg-gray-100 border-0 md:my-10 dark:bg-gray-700" />
-                <div className='p-4 rounded-full bg-slate-500 text-white'>
-                  08
-                </div>
-              </div>
-              <div className='flex items-center'>
-                <hr className="w-8 h-1 bg-gray-100 border-0 md:my-10 dark:bg-gray-700" />
-                <div className='p-4 rounded-full bg-slate-500 text-white'>
-                  09
-                </div>
-              </div>
-              <div className='flex items-center'>
-                <hr className="w-8 h-1 bg-gray-100 border-0 md:my-10 dark:bg-gray-700" />
-                <div className='p-4 rounded-full bg-slate-500 text-white'>
-                  10
-                </div>
-              </div>
-              <div className='flex items-center'>
-                <hr className="w-8 h-1 bg-gray-100 border-0 md:my-10 dark:bg-gray-700" />
-                <div className='p-4 rounded-full bg-slate-500 text-white'>
-                  11
-                </div>
-              </div>
-              <div className='flex items-center'>
-                <hr className="w-8 h-1 bg-gray-100 border-0 md:my-10 dark:bg-gray-700" />
-                <div className='p-4 rounded-full bg-slate-500 text-white'>
-                  12
-                </div>
-              </div>
+              {roadmap.tasks.map(task => {
+                if (task.id != '1') {
+                  return (
+                    <div key={task.id} className='flex items-center'>
+                      <hr className="w-8 h-1 bg-gray-100 border-0 md:my-10 dark:bg-gray-700" />
+                      <button
+                        id={task.id}
+                        className='p-4 rounded-full bg-slate-500 hover:bg-black focus:bg-green text-white'
+                        onClick={() => {
+                          setCurrentMilestone(task.id);
+                        }}
+                      >
+                        {task.id}
+                      </button>
+                    </div>
+                  );
+                } else {
+                  return (
+                    <div key={task.id} className='flex items-center'>
+                      <button
+                        id={task.id}
+                        className='p-4 rounded-full bg-slate-500 hover:bg-black focus:bg-green text-white'
+                        onClick={() => {
+                          setCurrentMilestone(task.id);
+                        }}
+                      >
+                        {task.id}
+                      </button>
+                    </div>
+                  );
+                }
+              })}
             </div>
           </div>
-          <div className='flex bg-white rounded-lg shadow-lg p-4'>
-            <div className='flex flex-col bg-white p-4 space-y-2 w-1/2'>
-              <div className='font-bold'>
-                Lorem Ipsum
-              </div>
-              <div className='text-sm'>
-                {description}
-              </div>
-              <div className='flex space-x-2'>
-                <div className='bg-green-100 grow font-bold text-center p-2'>
-                  Date-1 : ...........
-                </div>
-                <div className='bg-red-100 grow font-bold text-center p-2'>
-                  Date-2 : ...........
-                </div>
-              </div>
-            </div>
-            <div className='flex flex-col bg-white p-4 space-y-2 w-1/2 shrink-0'>
-              <div className='font-bold'>
-                Lorem Ipsum
-              </div>
-              <div className='flex flex-col justify-center space-y-2 text-sm'>
-                <div className='bg-yellow-100 p-2'>
-                  Sed maximus, lacus eget faucibus tempor
-                </div>
-                <div className='bg-orange-100 p-2'>
-                  Vestibulum turpis nibh, imperdiet non gravida quis
-                </div>
-                <div className='bg-red-100 p-2'>
-                  Phasellus in sollicitudin quam, id lacinia nisl.
-                </div>
-              </div>
-            </div>
-          </div>
+          <MilestoneDetails milestone={currentMilestone} />
         </div>
       </div>
     </div>
