37c4a273c124e35417b419f18b17336b0e24b358
ThanapatTr <6338098421@student.chula.ac.th>
2023-03-20 14:37:28 +0700

    Change & Add Roadmap parameters
    change:
    userName -> creator_name
    ownerName -> owner_name
    roadmapName -> title
    createdDate -> created_at
    updateDate -> edited_at
    views -> views_count
    
    add:
    owner_id
    creator_id
    rid
    stars_count
    forks_count

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -3,41 +3,52 @@ import PropTypes from 'prop-types';
 import placeholderImage from "../assets/roadmap_assets/Placeholder_Image.png"
 import { ReactComponent as EyeIcon } from "../assets/roadmap_assets/eye_Icon.svg"
 
-const Roadmap = ({ userName, ownerName, roadmapName, createdDate, updateDate, views }) => {
+const Roadmap = ({ owner_id, creator_id, owner_name, creator_name, rid, views_counts, stars_count, forks_count, created_at, edited_at, title }) => {
   Roadmap.propTypes = {
-    userName: PropTypes.string.isRequired,
-    ownerName: PropTypes.string.isRequired,
-    roadmapName: PropTypes.string.isRequired,
-    createdDate: PropTypes.string.isRequired,
-    updateDate: PropTypes.string.isRequired,
-    views: PropTypes.number.isRequired,
+    owner_id: PropTypes.number,
+    creator_id: PropTypes.number,
+    owner_name: PropTypes.string,
+    creator_name: PropTypes.string,
+    rid: PropTypes.number,
+    views_count: PropTypes.number,
+    stars_count: PropTypes.number,
+    forks_count: PropTypes.number,
+    created_at: PropTypes.string,
+    edited_at: PropTypes.string,
+    title: PropTypes.string
   };
   return (
-    <div className="relative rounded-[48px] bg-white border-gray-300 border-2 shadow-md w-80 h-80 m-8">
+    <div className="relative rounded-[48px] bg-white border-gray-300 border-2 shadow-md w-80 h-80">
       <div className="relative container rounded-3xl h-3/5 w-auto m-2">
         <img src={placeholderImage} className="relative object-cover rounded-[48px] h-full w-full" />
         <h3 className="absolute text-black bottom-2 left-6">
-          {userName}
+          {creator_name}
         </h3>
         <h6 className="absolute text-gray-600 bottom-2 right-6">
-          Last updated: {updateDate}
+          Last updated: {edited_at}
         </h6>
       </div>
       <div className="relative h-2/5 w-auto">
         <h1 className="absolute top-0 left-2 text-2xl">
-          {roadmapName}
+          {title}
         </h1>
+        <h6 className="absolute top-0 right-24" >
+          fork : {forks_count}
+        </h6>
+        <h6 className="absolute top-0 right-8">
+          star : {stars_count}
+        </h6>
         <h1 className="absolute top-10 left-2">
-          Owner: {ownerName}
+          Owner: {owner_name}
         </h1>
         <h1 className="absolute top-10 right-2 text-gray-600">
-          Created : {createdDate}
+          Created : {created_at}
         </h1>
         <span className="absolute bottom-6 left-8">
-            <EyeIcon />
+          <EyeIcon />
         </span>
         <h1 className="absolute bottom-6 left-16">
-          : {views} views
+          : {views_counts} views
         </h1>
       </div>
     </div>
@@ -45,56 +56,3 @@ const Roadmap = ({ userName, ownerName, roadmapName, createdDate, updateDate, vi
 };
 
 export default Roadmap;
-
-
-
-/*const roadmap1 = {userName: "FingTheMan", 
-              ownerName: "Wuttikorn", 
-              roadmapName: "Doing Capstone", 
-              createdDate: "01/01/2023", 
-              updateDate: "18/03/2023", 
-              views: 177013};
-const roadmap2 = {userName: "FingTheMan", 
-              ownerName: "Wuttikorn", 
-              roadmapName: "Dying from Capstone", 
-              createdDate: "19/02/2023", 
-              updateDate: "18/03/2023", 
-              views: 177013};
-
-const allRoadmap = [roadmap1, roadmap2, roadmap2]
-var roadmapList = []
-allRoadmap.forEach((roadmap)=>{
-  roadmapList.push(<div className="relative rounded-[48px] bg-white border-gray-300 border-2 shadow-md w-80 h-80 m-8">
-  <div className="relative container rounded-3xl h-3/5 w-auto m-2">
-    <img src={placeholderImage} className="relative object-cover rounded-[48px] h-full w-full" />
-    <h3 className="absolute text-black bottom-2 left-6">
-      {roadmap.userName}
-    </h3>
-    <h6 className="absolute text-gray-600 bottom-2 right-6">
-      Last updated: {roadmap.updateDate}
-    </h6>
-  </div>
-  <div className="relative h-2/5 w-auto">
-    <h1 className="absolute top-0 left-2 text-2xl">
-      {roadmap.roadmapName}
-    </h1>
-    <h1 className="absolute top-10 left-2">
-      Owner: {roadmap.ownerName}
-    </h1>
-    <h1 className="absolute top-10 right-2 text-gray-600">
-      Created : {roadmap.createdDate}
-    </h1>
-    <span className="absolute bottom-6 left-8">
-        <EyeIcon />
-    </span>
-    <h1 className="absolute bottom-6 left-16">
-      : {roadmap.views} views
-    </h1>
-  </div>
-</div>)
-})
-return (
-  <>
-    {roadmapList}
-  </>
-);*/
