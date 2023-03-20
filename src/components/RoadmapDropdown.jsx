5b09dd6399c78451d656ca603a4255fcbbaca366
wuttikorn.n45 <wuttikorn.n45@gmail.com>
2023-03-20 20:16:23 +0700

    make dropdown appear onClick

--- src/components/RoadmapDropdown.jsx
+++ src/components/RoadmapDropdown.jsx
@@ -1,4 +1,4 @@
-import react from "react"
+import react, { useState } from "react"
 import { ReactComponent as PrivateIcon } from "../assets/roadmapDropdown_assets/private_icon.svg"
 import { ReactComponent as PublicIcon } from "../assets/roadmapDropdown_assets/public_icon.svg"
 import { ReactComponent as DeleteIcon } from "../assets/roadmapDropdown_assets/delete_icon.svg"
@@ -6,24 +6,27 @@ import { ReactComponent as RenameIcon } from "../assets/roadmapDropdown_assets/r
 import { ReactComponent as DropdownIcon } from "../assets/roadmapDropdown_assets/dropdown_icon.svg"
 
 const RoadmapDropdown = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    var showDropdown = {display:"none"};
+
+    const handleClick = () => {
+        setIsOpen(!isOpen);
+    };
+
+    if (isOpen)
+        showDropdown = {display:"inline-block"};
+
     return (
         <>
-            <div className="absolute flex flex-col items-center bg-[#FFFFFF] font-bold appearance-none border rounded-xl px-12 py-4 ml-2 leading-tight focus:outline-none focus:shadow-outline w-60 h-36 z-50">
-                <div className="relative">
-                    This is dropdown
+            <button onClick={handleClick}>
+                <div className="absolute left-[690px] top-[320px] flex justify-center items-center w-[20px] h-[20px] rounded-[20px] hover:bg-[#EEEAEA]">
+                    <DropdownIcon/>
                 </div>
-                <span className="absolute left-0">
+            </button>
+            <div className="absolute left-[570px] top-[350px] bg-[#FFFFFF] font-bold appearance-none border rounded-xl px-12 py-4 ml-2 leading-tight focus:outline-none focus:shadow-outline w-60 h-36 z-50" style={showDropdown}>
+                <div className="absolute left-0">
                     <PrivateIcon/>
-                </span>
-                <span>
-                    <PublicIcon/>
-                </span>
-                <span>
-                    <DeleteIcon/>
-                </span>
-                <span>
-                    <RenameIcon/>
-                </span>
+                </div>
             </div>
         </>
     )
