24508d8206eddf1d2f5de3d9c23aea0ea465a57e
James <49901378+Mifuu@users.noreply.github.com>
2023-03-20 10:09:36 +0700

    WIP change tab working
    -Use parse function

--- src/components/SettingTab.jsx
+++ src/components/SettingTab.jsx
@@ -4,24 +4,24 @@ import { ReactComponent as HomeLogo } from "../assets/navbar_assets/home_icon.sv
 
 const TabItem = (props) => {
     return (
-        <div className='grow py-1 hover:bg-blue-500 hover:scale-105 transition duration-200'>
+        <button className='grow py-1 hover:bg-blue-500 hover:scale-105 transition duration-200' onClick={() => props.setTab(props.tabName)}>
             <div className='overflow-hidden whitespace-nowrap flex justify-center'>
                 <props.SvgIcon className='w-6 h-auto inline'/>
                 <div className='font-nunito-sans text-black font-bold justify-self-center'>
                     {props.displayName}
                 </div>
             </div>
-        </div>
+        </button>
     )
 }
 
-const SettingTab = () => {
+const SettingTab = (props) => {
     return (
         <div className='flex'>
             <div className='flex justify-evenly grow'>
-                <TabItem SvgIcon={HomeLogo} displayName="Profile"/>
-                <TabItem SvgIcon={HomeLogo} displayName="Account"/>
-                <TabItem SvgIcon={HomeLogo} displayName="Appearance"/>
+                <TabItem SvgIcon={HomeLogo} displayName="Profile" tabName="profile" setTab={props.setTab}/>
+                <TabItem SvgIcon={HomeLogo} displayName="Account" tabName="account" setTab={props.setTab}/>
+                <TabItem SvgIcon={HomeLogo} displayName="Appearance" tabName="appearance" setTab={props.setTab}/>
             </div>
             <div className='grow'>
 
--- src/pages/Setting.jsx
+++ src/pages/Setting.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 import SettingTab from '../components/SettingTab';
 
 const Setting = () => {
+    const [tab, setTab] = useState("");
+    
     return (
         <div className='flex flex-col'>
-            <SettingTab/>
+            <SettingTab tab={tab} setTab={setTab}/>
+            <RenderSettingContent tab={tab}/>
         </div>
     )
+}
+
+function RenderSettingContent({tab}) {
+    switch (tab) {
+        case "profile":
+            return <p>profile</p>
+        case "account":
+            return <p>account</p>
+        case "appearance":
+            return <p>appearance</p>
+    }
 }
 
 export default Setting;
