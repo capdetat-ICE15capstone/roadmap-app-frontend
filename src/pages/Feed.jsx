f518041de23263cdaa0a98a8bf30204e3328bd3f
ThanapatTr <6338098421@student.chula.ac.th>
2023-03-20 18:37:33 +0700

    Feed can now show roadmaps using data from simulated external api
    simulated data:
     "multipleRoadmaps":[
          { "roadmapArray":[
            { "id": 0, "creator_name": "user13", "owner_name": "user1", "title": "Roadmap 1", "created_at": "3/18/2013", "edited_at": "Today", "views_counts": "1000" },
            { "id": 1, "creator_name": "user14", "owner_name": "user2", "title": "Roadmap 2", "created_at": "3/18/2013", "edited_at": "Today", "views_counts": "2000" },
            { "id": 2, "creator_name": "user15", "owner_name": "user3", "title": "Roadmap 3", "created_at": "3/18/2013", "edited_at": "Today", "views_counts": "3000" },
            { "id": 3, "creator_name": "user16", "owner_name": "user4", "title": "Roadmap 4", "created_at": "3/18/2013", "edited_at": "Today", "views_counts": "4000" },
            { "id": 4, "creator_name": "user17", "owner_name": "user5", "title": "Roadmap 5", "created_at": "3/18/2013", "edited_at": "Today", "views_counts": "5000" },
            { "id": 5, "creator_name": "user18", "owner_name": "user6", "title": "Roadmap 6", "created_at": "3/18/2013", "edited_at": "Today", "views_counts": "6000" },
            { "id": 6, "creator_name": "user19", "owner_name": "user7", "title": "Roadmap 7", "created_at": "3/18/2013", "edited_at": "Today", "views_counts": "7000" },
            { "id": 7, "creator_name": "user20", "owner_name": "user8", "title": "Roadmap 8", "created_at": "3/18/2013", "edited_at": "Today", "views_counts": "8000" },
            { "id": 8, "creator_name": "user21", "owner_name": "user9", "title": "Roadmap 9", "created_at": "3/18/2013", "edited_at": "Today", "views_counts": "9000" },
            { "id": 9, "creator_name": "user22", "owner_name": "user10", "title": "Roadmap 10", "created_at": "3/18/2013", "edited_at": "Today", "views_counts": "10000" },
            { "id": 10, "creator_name": "user23", "owner_name": "user11", "title": "Roadmap 11", "created_at": "3/18/2013", "edited_at": "Today", "views_counts": "11000" },
            { "id": 11, "creator_name": "user24", "owner_name": "user12", "title": "Roadmap 12", "created_at": "3/18/2013", "edited_at": "Today", "views_counts": "12000" }
          ]
          }
        ]

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,58 +1,78 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from 'react';
+import Roadmap from "../components/Roadmap";
 import SearchBar from "../components/SearchBar";
 import SearchButton from "../components/Button";
-import Roadmap from "../components/Roadmap";
 
 const Feed = () => {
+
   const [search, setSearch] = useState("");
+  const [roadmapArray, setRoadmapArray] = useState([]);
+  const [page, setPage] = useState(1);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("search: " + document.getElementById("roadmapName").value);
+    console.log("search: " + document.getElementById("InputSearch").value);
   };
+
+  //fetch roadmap data from API
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`http://localhost:3000/multipleRoadmaps`); //`http://localhost:3000/multipleRoadmaps=${page}`
+      const data = await response.json();
+      let newArray = [];
+      data.forEach((data) => {
+        newArray = newArray.concat(data.roadmapArray);
+      });     
+      setRoadmapArray(prevArray => [...prevArray, ...newArray]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [page]);
+
+  //infinite scroll functionality
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight) return;
+      setPage(prevPage => prevPage + 1);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <>
-    {/*Search bar*/}
-    <form className="mx-auto" id="searchForm" onSubmit={handleSubmit}>
-      <div
-        className="relative inline-block my-4"
-        onSubmit={handleSubmit}
-      >
-        <SearchBar/>
-        <SearchButton/>
+      {/*Search bar*/}
+      <form className="mx-auto" id="searchForm" onSubmit={handleSubmit}>
+        <div
+          className="relative inline-block my-4"
+          onSubmit={handleSubmit}
+        >
+          <SearchBar />
+          <SearchButton />
+        </div>
+      </form>
+      {/*Search Result*/}
+      <div className="flex flex-wrap mx-4">
+        {roadmapArray.map((roadmap, index) => (
+          <div key={index} className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-2">
+            <Roadmap
+              creator_name={roadmap.creator_name}
+              owner_name={roadmap.owner_name}
+              title={roadmap.title}
+              created_at={roadmap.created_at}
+              edited_at={roadmap.edited_at}
+              views_counts={roadmap.views_counts}
+            />
+          </div>
+        ))}
       </div>
-    </form>
-    <div className="relative inline-block mx-4">
-      <div className="inline-block">
-        <Roadmap
-        userName="BomberMan"
-        ownerName="Unknown" 
-        roadmapName="Commiting Suicide" 
-        createdDate="10/26/2022" 
-        updateDate="02/16/2023" 
-        views={69} />
-      </div>
-      <div className="inline-block">
-        <Roadmap 
-        userName="OctopusLarry"
-        ownerName="Fing" 
-        roadmapName="Downloading Minecraft" 
-        createdDate="04/26/2020" 
-        updateDate="07/16/2021" 
-        views={420} />
-      </div>
-      <div className="inline-block">
-        <Roadmap
-        userName="Lonely_Fing" 
-        ownerName="Nantawitaya" 
-        roadmapName="Playing Paladins" 
-        createdDate="05/02/2018" 
-        updateDate="03/18/2023" 
-        views={177013} />
-      </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
 export default Feed;
