3dfa60663397c952f4c51ce7059b43ca411fd969
wuttikorn.n45 <wuttikorn.n45@gmail.com>
2023-03-21 17:17:49 +0700

    just implements incoming useState

--- src/components/RoadmapDropdown.jsx
+++ src/components/RoadmapDropdown.jsx
@@ -8,6 +8,9 @@ import { ReactComponent as DropdownIcon } from "../assets/roadmapDropdown_assets
 const RoadmapDropdown = () => {
     const [isOpen, setIsOpen] = useState(false);
     const [isPublic, setIsPublic] = useState(false);
+    const [isDeleteClick, setIsDeleteClick] = useState(false);
+    const [isRenameClick, setIsRenameClick] = useState(false);
+
     var showDropdown = {display:"none"};
     var togglePrivacy = {transform:"translateX(0px)"};
     var toggleButtonColor = {background:"#736666"}
@@ -20,6 +23,14 @@ const RoadmapDropdown = () => {
         setIsPublic(!isPublic)
     }
 
+    const wantToDelete = () => {
+
+    }
+
+    const wantToRename = () => {
+        
+    }
+
     if (isOpen)
         showDropdown = {display:"inline-block"};
 
@@ -29,45 +40,47 @@ const RoadmapDropdown = () => {
     }
 
     return (
-        <div className="absolute flex justify-center z-50">
-            <button onClick={handleClick} className="relative flex justify-center items-center top-[-70px] left-[310px] w-[20px] h-[20px] rounded-[20px] hover:bg-[rgb(238,234,234)]">                
-                <DropdownIcon/>
-            </button>
-            <div className="absolute top-[-30px] left-[200px] bg-[#FFFFFF] font-bold appearance-none border rounded-xl leading-tight focus:outline-none focus:shadow-outline w-60 h-36" style={showDropdown}>
-                <div className="inline-block relative">
-                    <div className="absolute left-[14px] top-[0px]">
-                        <PrivateIcon/>
+        <>
+            <div className="absolute flex justify-center z-50">
+                <button onClick={handleClick} className="relative flex justify-center items-center top-[-70px] left-[310px] w-[20px] h-[20px] rounded-[20px] hover:bg-[rgb(238,234,234)]">                
+                    <DropdownIcon/>
+                </button>
+                <div className="absolute top-[-30px] left-[200px] bg-[#FFFFFF] font-bold appearance-none border rounded-xl leading-tight focus:outline-none focus:shadow-outline w-60 h-36" style={showDropdown}>
+                    <div className="inline-block relative">
+                        <div className="absolute left-[14px] top-[0px]">
+                            <PrivateIcon/>
+                        </div>
+                        <div className="absolute left-[37px] top-[-1px]">
+                            Private
+                        </div>
+                        <button onClick={roadmapPrivacy} className="absolute left-[96px] top-[4px] w-[14px] h-[14px] bg-[#FFFFFF] rounded-[14px] border-[0.7px] border-solid border-[#D9D9D9] hover:scale-[1.15] z-50" style={togglePrivacy}></button>
+                        <button onClick={roadmapPrivacy} className="absolute left-[96px] top-[6px] w-[33px] h-[10px] bg-[#736666] rounded-[50px]" style={toggleButtonColor}></button>
+                        <div className="absolute left-[134px] top-[0px]">
+                            <PublicIcon/>
+                        </div>
+                        <div className="absolute left-[152px] top-[-1px]">
+                            Public
+                        </div>
                     </div>
-                    <div className="absolute left-[37px] top-[-1px]">
-                        Private
-                    </div>
-                    <button onClick={roadmapPrivacy} className="absolute left-[96px] top-[4px] w-[14px] h-[14px] bg-[#FFFFFF] rounded-[14px] border-[0.7px] border-solid border-[#D9D9D9] hover:scale-[1.15] z-50" style={togglePrivacy}></button>
-                    <button onClick={roadmapPrivacy} className="absolute left-[96px] top-[6px] w-[33px] h-[10px] bg-[#736666] rounded-[50px]" style={toggleButtonColor}></button>
-                    <div className="absolute left-[134px] top-[0px]">
-                        <PublicIcon/>
-                    </div>
-                    <div className="absolute left-[152px] top-[-1px]">
-                        Public
-                    </div>
-                </div>
-                <div className="inline-block relative">
-                    <div className="absolute left-[13px] top-[35px]">
-                        <DeleteIcon/>
-                    </div>
-                    <div className="absolute left-[37px] top-[34px]">
-                        Delete
-                    </div>
-                </div>
-                <div className="inline-block relative">
-                    <div className="absolute left-[13px] top-[70px]">
-                        <RenameIcon/>
-                    </div>
-                    <div className="absolute left-[37px] top-[69px]">
-                        Rename
-                    </div>
-                </div>
-            </div>            
-        </div>
+                    <button className="inline-block relative hover:bg-[rgb(128 128 128)]">
+                        <div className="absolute left-[13px] top-[35px]">
+                            <DeleteIcon/>
+                        </div>
+                        <div className="absolute left-[37px] top-[34px]">
+                            Delete
+                        </div>
+                    </button>
+                    <button className="inline-block relative hover:bg-[rgb(128 128 128)]">
+                        <div className="absolute left-[13px] top-[70px]">
+                            <RenameIcon/>
+                        </div>
+                        <div className="absolute left-[37px] top-[69px]">
+                            Rename
+                        </div>
+                    </button>
+                </div>            
+            </div>
+        </>
     )
 }
 
