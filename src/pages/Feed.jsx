386a1580467a60ff2fc7063b851ab92b1b07c2e2
ThanapatTr <6338098421@student.chula.ac.th>
2023-03-23 23:21:58 +0700

    Set page +1 when scrolled to the bottom

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Roadmap from "../components/Roadmap";
 import SearchBar from "../components/SearchBar";
 
@@ -7,6 +7,7 @@ const Feed = () => {
   const [search, setSearch] = useState("");
   const [roadmapArray, setRoadmapArray] = useState([]);
   const [page, setPage] = useState(1);
+  const containerRef = useRef(null);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -32,16 +33,14 @@ const Feed = () => {
     fetchData();
   }, [page]);
 
-  //infinite scroll functionality
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight) return;
+  // set page +1 when scrolled to the bottom
+  function handleScroll() {
+    const container = containerRef.current;
+    if (container.scrollTop + container.clientHeight >= container.scrollHeight) {
       setPage(prevPage => prevPage + 1);
-    };
-
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
+      console.log('Scrolled to bottom');
+    }
+  }
 
   return (
     <>
@@ -52,13 +51,13 @@ const Feed = () => {
           onSubmit={handleSubmit}
         >
           <SearchBar />
-          <button type="submit"className="bg-[#00286E] hover:bg-[#011C4B] text-white font-bold appearance-none border rounded-3xl px-12 py-4 ml-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+          <button type="submit" className="bg-[#00286E] hover:bg-[#011C4B] text-white font-bold appearance-none border rounded-3xl px-12 py-4 ml-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
             Search
           </button>
         </div>
       </form>
       {/*Search Result*/}
-      <div className="relative flex flex-wrap mx-4 overflow-y-scroll">
+      <div className="relative flex flex-wrap mx-4 overflow-y-scroll " onScroll={handleScroll} ref={containerRef}>
         {roadmapArray.map((roadmap, index) => (
           <div key={index} className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-2">
             <Roadmap
@@ -78,3 +77,30 @@ const Feed = () => {
 
 export default Feed;
 //npx json-server --watch json_server_test/db.json
+
+//infinite scroll functionality
+  /*
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight) return;
+      setPage(prevPage => prevPage + 1);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+  */
+
+  /*
+  useEffect(() => {
+    const handleScroll = () => {
+      const container = containerRef.current;
+      if (container.scrollTop + container.clientHeight >= container.scrollHeight) return;
+      setPage(prevPage => prevPage + 1);
+      console.log('Scrolled to bottom');
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+  */
