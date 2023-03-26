ee6cc96cbd5acea246903b085a1eea205b7ad25c
Hambaiki <92420824+Hambaiki@users.noreply.github.com>
2023-03-26 23:59:39 +0700

    Modification and Clean up of both Login.jsx and Signup.jsx (unfinished)

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,139 +1,100 @@
 import React, { useState } from 'react'
-import { useNavigate, Link } from 'react-router-dom';
 import PropTypes, { func } from 'prop-types';
-
-import { ReactComponent as Logo } from "../assets/logo-big.svg"
+import Signup from './Signup';
 
 async function loginUser(credentials) {
-    return fetch('http://localhost:5173/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(credentials)
-    })
-        .then(data => data.json())
+  return fetch('http://localhost:5173/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(credentials)
+  })
+    .then(data => data.json())
 }
 
 export default function Login({ setToken }) {
-    // const navigate = useNavigate();
+  // const navigate = useNavigate();
+  const [signupVisibility, setSingupVisibility] = useState(false);
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [rememberMe, setRememberMe] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rememberMe, setRememberMe] = useState(false);
 
-    const handleSubmit = async e => {
-        e.preventDefault();
-        const token = await loginUser({
-            email,
-            password
-        });
-        setToken(token);
-    };
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const token = await loginUser({
+      email,
+      password
+    });
+    setToken(token);
+  };
 
-    return (
-        <>
-            <div className="flex flex-col h-screen bg-gray-200">
-                <div className="flex bg-white rounded shadow-xl m-auto">
-                    {/* <div className="flex flex-col bg-gradient-to-b from-teal-300 to-amber-100 rounded">
-                        <div className="flex flex-col grow justify-center my-4 mx-12">
-                            <Logo className="justify-center self-center" />
-                            <div className="mb-2">
-                                <p className="font-semibold text-2xl ">
-                                    WELCOME to
-                                </p>
-                                <p className="font-extrabold text-3xl ">
-                                    APPNAME!
-                                </p>
-                            </div>
-                            <div className="mb-4">
-                                <p className="font-regular text-sm">
-                                    We're here to help YOU complete your <br />
-                                    TASK successfully
-                                </p>
-                            </div>
-                        </div>
-                    </div> */}
-                    <div className="flex flex-col m-6">
-                        <p className="flex justify-center mb-4 text-3xl font-bold text-slate-600">
-                            User Login
-                        </p>
-                        <form id="login-form" onSubmit={handleSubmit}>
-                            <div className="flex flex-col space-y-4 mb-4">
-                                <input
-                                    type="email"
-                                    className="shadow appearance-none border rounded-3xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                    id="InputEmail"
-                                    placeholder="Email"
-                                    onChange={(event) =>
-                                        setEmail(event.target.value)
-                                    }
-                                />
-                                <input
-                                    type="password"
-                                    className="shadow appearance-none border rounded-3xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                    id="InputPassword"
-                                    placeholder="********"
-                                    onChange={(event) =>
-                                        setPassword(event.target.value)
-                                    }
-                                />
-                            </div>
-                            <div className="flex flex-col mb-4">
-                                <div className="flex flex-row justify-between ">
-                                    <label>
-                                        <input
-                                            type="checkbox"
-                                            className="w-4 h-4S bg-gray-100 border-gray-300 rounded"
-                                            id="CheckRememberMe"
-                                            onChange={() => setRememberMe(!rememberMe)}
-                                        />
-                                        <a className="ml-2 text-xs font-medium text-gray-400">
-                                            Remember Me
-                                        </a>
-                                    </label>
-                                    <div>
-                                        <a className="font-bold text-xs text-blue-900" href="#">
-                                            Forgot Password?
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="flex flex-col space-y-2">
-                                <button type="submit" className="bg-blue-900 text-white shadow font-bold py-2 rounded-lg">
-                                    Log In
-                                </button>      
-                            </div>
-                        </form>
-                        {/* <div className="flex justify-center m-4">
-                            <p className="text-xs font-light text-gray-400">
-                                ---------------- OR ----------------
-                            </p>
-                        </div>
-                        <div className="flex flex-col mb-2">
-                            <div className="flex items-center justify-center mb-4">
-                                <button className="bg-transparent grow text-gray-600 border border-grey-500 py-2 font-semilight rounded-lg text-sm" type="button">
-                                    Continue with Google
-                                </button>
-                            </div>
-                            <div className="flex items-center justify-center mb-4">
-                                <button className="bg-transparent grow text-gray-600 border border-grey-500 py-2 font-semilight rounded-lg text-sm" type="button">
-                                    Continue with Facebook
-                                </button>
-                            </div>
-                            <div className="flex items-center justify-center mb-4">
-                                <button className="bg-transparent grow text-gray-600 border border-grey-500 py-2 font-semilight rounded-lg text-sm" type="button">
-                                    Continue with Email
-                                </button>
-                            </div>
-                        </div> */}
-                    </div>
+  return (
+    <>
+      <div className="flex flex-col h-screen bg-gray-200 relative">
+        <div className="flex bg-white rounded shadow-xl m-auto">
+          <div className="flex flex-col m-6">
+            <p className="flex justify-center mb-4 text-3xl font-bold text-slate-600">
+              User Login
+            </p>
+            <form id="login-form" onSubmit={handleSubmit}>
+              <div className="flex flex-col space-y-4 mb-4">
+                <input
+                  type="email"
+                  className="shadow appearance-none border rounded-3xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="InputEmail"
+                  placeholder="Email"
+                  onChange={(event) =>
+                    setEmail(event.target.value)
+                  }
+                />
+                <input
+                  type="password"
+                  className="shadow appearance-none border rounded-3xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="InputPassword"
+                  placeholder="********"
+                  onChange={(event) =>
+                    setPassword(event.target.value)
+                  }
+                />
+              </div>
+              <div className="flex flex-col mb-4">
+                <div className="flex flex-row justify-between ">
+                  <label>
+                    <input
+                      type="checkbox"
+                      className="w-4 h-4S bg-gray-100 border-gray-300 rounded"
+                      id="CheckRememberMe"
+                      onChange={() => setRememberMe(!rememberMe)}
+                    />
+                    <a className="ml-2 text-xs font-medium text-gray-400">
+                      Remember Me
+                    </a>
+                  </label>
+                  <div>
+                    <a className="font-bold text-xs text-blue-900" href="#">
+                      Forgot Password?
+                    </a>
+                  </div>
                 </div>
-            </div>
-        </>
-    )
+              </div>
+              <div className="flex flex-col space-y-2">
+                <button type="submit" className="bg-blue-900 text-white shadow font-bold py-2 rounded-lg">
+                  Log In
+                </button>
+                <button type="button" onClick={() => setSingupVisibility(!signupVisibility)} className="bg-red-900 text-white shadow font-bold py-2 rounded-lg">
+                  Sign Up
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 Login.propTypes = {
-    setToken: PropTypes.func.isRequired
+  setToken: PropTypes.func.isRequired
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 
-const Signup = () => {
-  const navigate = useNavigate();
+function Signup() {
 
   const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
 
@@ -61,7 +59,7 @@ const Signup = () => {
       console.log("agreement must be set to TRUE");
       return;
     }
-    
+
     const res = {
       'email': email,
       'password': password,
@@ -75,168 +73,138 @@ const Signup = () => {
 
   return (
     <>
-      <div className="flex flex-col h-screen bg-gray-200">
-        <div className="flex bg-white rounded shadow-lg max-w-3xl m-auto">
-          <div className="flex flex-col m-8">
-            <p className="text-center mb-4 text-3xl font-bold text-slate-600">
-              Create Account
-            </p>
-            <form id="register-form" onSubmit={handleSubmit}>
-              <div className="flex flex-col space-y-2 mb-4">
-              <div className="flex flex-col">
-                  <label>
-                    <p className="text-gray-600 text-xs mb-1">
-                      Email Address
-                    </p>
-                    <input
-                      type="email"
-                      className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                      id="InputEmail"
-                      placeholder="Email Address"
-                      onChange={(event) =>
-                        setEmail(event.target.value)
-                      }
-                    />
-                  </label>
+      <div className={`flex flex-col h-screen w-screen absolute ${(props.visible) ? 'visible' : 'hidden'}`}>
+        <div className="flex flex-col h-screen bg-black bg-opacity-50">
+          <div className="flex bg-white rounded shadow-lg max-w-3xl m-auto">
+            <div className="flex flex-col m-8">
+              <p className="text-center mb-4 text-3xl font-bold text-slate-600">
+                Create Account
+              </p>
+              <form id="register-form" onSubmit={handleSubmit}>
+                <div className="flex flex-col space-y-2 mb-4">
+                  <div className="flex flex-col">
+                    <label>
+                      <p className="text-gray-600 text-xs mb-1">
+                        Email Address
+                      </p>
+                      <input
+                        type="email"
+                        className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="InputEmail"
+                        placeholder="Email Address"
+                        onChange={(event) =>
+                          setEmail(event.target.value)
+                        }
+                      />
+                    </label>
+                  </div>
+                  <div className="flex flex-row justify-between space-x-4">
+
+                    <label>
+                      <p className="text-gray-600 text-xs mb-1">
+                        First Name
+                      </p>
+                      <input
+                        type="firstname"
+                        className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="InputFirstName"
+                        placeholder="First Name"
+                        onChange={(event) =>
+                          setFirstName(event.target.value)
+                        }
+                      />
+                    </label>
+                    <label>
+                      <p className="text-gray-600 text-xs mb-1">
+                        Last Name
+                      </p>
+                      <input
+                        type="lastname"
+                        className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="InputLastName"
+                        placeholder="Last Name"
+                        onChange={(event) =>
+                          setLastName(event.target.value)
+                        }
+                      />
+                    </label>
+                  </div>
+                  <div className="flex flex-col">
+                    <label>
+                      <p className="text-gray-600 text-xs mb-1">
+                        Username
+                      </p>
+                      <input
+                        type="username"
+                        className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="InputUsername"
+                        placeholder="Username"
+                        onChange={(event) =>
+                          setUsername(event.target.value)
+                        }
+                      />
+                    </label>
+                  </div>
+                  <div className="flex flex-row justify-between space-x-4">
+                    <label>
+                      <p className="text-gray-600 text-xs mb-1">
+                        Password
+                      </p>
+                      <input
+                        type="password"
+                        className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="InputPassword"
+                        placeholder="********"
+                        onChange={(event) =>
+                          setPassword(event.target.value)
+                        }
+                      />
+                    </label>
+                    <label>
+                      <p className="text-gray-600 text-xs mb-1">
+                        Confirm Password
+                      </p>
+                      <input
+                        type="password"
+                        className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="InputPasswordConfirm"
+                        placeholder="********"
+                        onChange={(event) =>
+                          setPasswordConfirm(event.target.value)
+                        }
+                      />
+                    </label>
+                  </div>
                 </div>
-                <div className="flex flex-row justify-between space-x-4">
-                  
-                  <label>
-                    <p className="text-gray-600 text-xs mb-1">
-                      First Name
-                    </p>
+                <div className="flex flex-col space-y-4">
+                  <label className="ml-2 text-xs font-medium text-gray-400">
                     <input
-                      type="firstname"
-                      className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                      id="InputFirstName"
-                      placeholder="First Name"
-                      onChange={(event) =>
-                        setFirstName(event.target.value)
-                      }
+                      type="checkbox"
+                      className="w-4 h-4S mr-1 bg-gray-100 border-gray-300 rounded"
+                      id="CheckAgree"
+                      onChange={() => setAgreement(!agreement)}
                     />
+                    I have agreed to
+                    <a href="#" className="ml-1 text-xs font-bold text-gray-400">
+                      ayayayayayaya
+                    </a>
                   </label>
-                  <label>
-                    <p className="text-gray-600 text-xs mb-1">
-                      Last Name
-                    </p>
-                    <input
-                      type="lastname"
-                      className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                      id="InputLastName"
-                      placeholder="Last Name"
-                      onChange={(event) =>
-                        setLastName(event.target.value)
-                      }
-                    />
-                  </label>
+                  <button type="submit" className="bg-red-500 text-white shadow font-bold py-2 grow rounded-3xl focus:outline-none focus:shadow-outline">
+                    Sign Up
+                  </button>
                 </div>
-                <div className="flex flex-col">
-                  <label>
-                    <p className="text-gray-600 text-xs mb-1">
-                      Username
-                    </p>
-                    <input
-                      type="username"
-                      className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                      id="InputUsername"
-                      placeholder="Username"
-                      onChange={(event) =>
-                        setUsername(event.target.value)
-                      }
-                    />
-                  </label>
-                </div>
-                <div className="flex flex-row justify-between space-x-4">
-                  <label>
-                    <p className="text-gray-600 text-xs mb-1">
-                      Password
-                    </p>
-                    <input
-                      type="password"
-                      className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                      id="InputPassword"
-                      placeholder="********"
-                      onChange={(event) =>
-                        setPassword(event.target.value)
-                      }
-                    />
-                  </label>
-                  <label>
-                    <p className="text-gray-600 text-xs mb-1">
-                      Confirm Password
-                    </p>
-                    <input
-                      type="password"
-                      className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                      id="InputPasswordConfirm"
-                      placeholder="********"
-                      onChange={(event) =>
-                        setPasswordConfirm(event.target.value)
-                      }
-                    />
-                  </label>
-                </div>
-              </div>
-              <div className="flex flex-col space-y-4">
-                <label className="ml-2 text-xs font-medium text-gray-400">
-                  <input
-                    type="checkbox"
-                    className="w-4 h-4S mr-1 bg-gray-100 border-gray-300 rounded"
-                    id="CheckAgree"
-                    onChange={() => setAgreement(!agreement)}
-                  />
-                  I have agreed to
-                  <a href="#" className="ml-1 text-xs font-bold text-gray-400">
-                    ayayayayayaya
-                  </a>
-                </label>
-                <button type="submit" className="bg-red-500 text-white shadow font-bold py-2 grow rounded-3xl focus:outline-none focus:shadow-outline">
-                  Sign Up
+              </form>
+              <div className="flex justify-center mt-4">
+                <button type="button" className="text-xs font-light text-gray-400">
+                  Alrady have an account?
                 </button>
               </div>
-            </form>
-            <div className="flex justify-center mt-4">
-              <button className="text-xs font-light text-gray-400" onClick={() => navigate("/login")}>
-                Alrady have an account?
-              </button>
             </div>
-            {/* <div className="flex justify-between mb-2 mx-8 space-x-2">
-              <div className="flex flex-auto">
-                <button className="bg-transparent grow text-gray-600 border border-grey-500 py-2 font-semilight rounded-3xl text-sm" type="button">
-                  <div className="flex justify-center items-center space-x-2">
-                    <img src={google} alt="google" className="w-4 h-4"></img>
-                    <span>Google</span>
-                  </div>
-                </button>
-              </div>
-              <div className="flex flex-auto">
-                <button className="bg-transparent grow text-gray-600 border border-grey-500 py-2 font-semilight rounded-3xl text-sm" type="button">
-                  <div className="flex justify-center items-center space-x-2">
-                    <img src={facebook} alt="google" className="w-4 h-4"></img>
-                    <span>Facebook</span>
-                  </div>
-                </button>
-              </div>
-              <div className="flex flex-auto">
-                <button className="bg-transparent grow text-gray-600 border border-grey-500 py-2 font-semilight rounded-3xl text-sm" type="button">
-                  <div className="flex justify-center items-center space-x-2">
-                    <img src={email} alt="google" className="w-4 h-4"></img>
-                    <span>Email</span>
-                  </div>
-                </button>
-              </div>
-            </div> */}
           </div>
-          {/* <div className="flex flex-col bg-gradient-to-b from-teal-300 to-amber-100 rounded">
-            <div className="flex flex-col grow justify-center my-4 mx-12">
-              <Logo className="justify-center self-center" />
-            </div>
-          </div> */}
         </div>
       </div>
     </>
   )
 }
-
 
 export default Signup
