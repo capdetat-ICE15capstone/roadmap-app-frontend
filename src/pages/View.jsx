65a559a8b32f99613f8599c8cb65bb1409f42d47
Hambaiki <92420824+Hambaiki@users.noreply.github.com>
2023-03-28 20:06:02 +0700

    Update View.jsx

--- src/pages/View.jsx
+++ src/pages/View.jsx
@@ -1,181 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
 
-function View() {
+export default function View() {
   const navigate = useNavigate();
-  
+
   const [currentMilestone, setCurrentMilestone] = useState('1');
   const [currentDetail, setCurrentDetail] = useState("");
   const [detailToggle, setCurrentDetailToggle] = useState(true);
-
-  function MilestoneDetails(props) {
-    const current = parseInt(props.milestone);
-    return (
-      <div className='flex flex-col bg-gray-100 rounded-2xl shadow-sm space-y-4'>
-        <div className='flex flex-row space-x-4 justify-between'>
-          <div className='flex flex-col space-y-2 w-1/2 p-4'>
-            <div className='font-bold'>
-              {roadmap.tasks[current - 1].name}
-            </div>
-            <div className='text-sm'>
-              {roadmap.tasks[current - 1].description}
-            </div>
-            <div className='flex space-x-2'>
-              <div className='bg-green-100 grow font-bold text-center p-2'>
-                {roadmap.tasks[current - 1].startDate}
-              </div>
-              <div className='bg-red-100 grow font-bold text-center p-2'>
-                {roadmap.tasks[current - 1].dueDate}
-              </div>
-            </div>
-          </div>
-          <div className='flex flex-col space-y-2 w-1/2 p-4 bg-gray-50 rounded'>
-            <div className='font-bold'>
-              Sub-Tasks
-            </div>
-            <div className='flex flex-col justify-center space-y-2 text-sm'>
-              {roadmap.tasks[current - 1].subtasks.map(subtask => {
-                return (
-                  <label key={subtask.id}>
-                    <input
-                      type="checkbox"
-                      className="w-4 h-4 mr-2 bg-gray-100 border-gray-300 rounded"
-                      defaultChecked={subtask.status ? true : false}
-                      onChange={() => {
-                        let res = { ...roadmap };
-                        res.tasks[current - 1].subtasks[subtask.id - 1].status = !subtask.status;
-                        setRoadmap(res);
-                      }
-                      }
-                    />
-                    {subtask.detail}
-                  </label>
-                )
-              })}
-            </div>
-          </div>
-        </div>
-      </div>
-    )
-  }
-
-  function RoadmapDetails(props) {
-    return (
-      <>
-        <div className='flex justify-between items-center'>
-          <div className='text-3xl font-extrabold'>
-            {roadmap.name}
-          </div>
-          <div className="flex space-x-1">
-            <button onClick={()=>console.log("like!")} className="bg-sub-blue grow text-white px-4 py-2 font-semilight rounded-full text-sm font-bold" type="button">
-              Like
-            </button>
-            <button onClick={()=>navigate("/edit", {'state': {'roadmap' : roadmap}})} className="bg-gray-300 grow text-white px-4 py-2 font-semilight rounded-full text-sm font-bold" type="button">
-              Edit
-            </button>
-            <button onClick={() => setCurrentDetailToggle(!detailToggle)} className="bg-gray-300 grow text-white px-4 py-2 font-semilight rounded-full text-sm font-bold" type="button">
-              i
-            </button>
-          </div>
-        </div>
-        <div className={`${(detailToggle) ? 'visible' : 'hidden'}`}>
-          <div className='flex flex-col bg-gray-100 rounded-2xl shadow-sm p-4 text-sm space-y-2'>
-            <div className='font-bold'>
-              Publicity: {roadmap.publicity ? "Public" : "Private"}
-            </div>
-            <div>
-              {roadmap.description}
-            </div>
-          </div>
-        </div>
-      </>
-    )
-  }
-
-  function RoadmapDisplay(props) {
-    const roadmap_props = props.roadmap;
-
-    return (
-      <div className='flex bg-gray-100 rounded-2xl shadow-sm p-4 overflow-x-auto'>
-        <div className='flex m-8'>
-          {roadmap_props.tasks.map(task => {
-            const subtasks = task.subtasks;
-            let currentActiveTask = '1';
-            let status = true;
-            for (var i = 0; i < subtasks.length; i++) {
-              if (subtasks[i].status === false) {
-                status = false;
-                currentActiveTask = task.id;
-                break;
-              }
-            }
-            if (status === true) {
-              if (task.id != '1') {
-                return (
-                  <div key={task.id} className='flex items-center'>
-                    <hr className="w-8 h-1 bg-gray-400 border-0 md:my-10" />
-                    <button
-                      id={task.id}
-                      className={`${(task.id === currentMilestone) ? 'bg-gray-800' : 'bg-gray-400'} hover:bg-black p-4 rounded-full focus:bg-green text-white`}
-                      onClick={() => {
-                        setCurrentMilestone(task.id);
-                      }}
-                    >
-                      {task.id}
-                    </button>
-                  </div>
-                );
-              } else {
-                return (
-                  <div key={task.id} className='flex items-center'>
-                    <button
-                      id={task.id}
-                      className={`${(task.id === currentMilestone) ? 'bg-gray-800' : 'bg-gray-400'} hover:bg-black p-4 rounded-full focus:bg-green text-white`}
-                      onClick={() => {
-                        setCurrentMilestone(task.id);
-                      }}
-                    >
-                      {task.id}
-                    </button>
-                  </div>
-                );
-              }
-            }
-            if (task.id != '1') {
-              return (
-                <div key={task.id} className='flex items-center'>
-                  <hr className="w-8 h-1 bg-gray-400 border-0 md:my-10" />
-                  <button
-                    id={task.id}
-                    className={`${(task.id === currentMilestone) ? 'bg-gray-800' : 'bg-gray-600'} hover:bg-black p-4 rounded-full focus:bg-green text-white`}
-                    onClick={() => {
-                      setCurrentMilestone(task.id);
-                    }}
-                  >
-                    {task.id}
-                  </button>
-                </div>
-              );
-            } else {
-              return (
-                <div key={task.id} className='flex items-center'>
-                  <button
-                    id={task.id}
-                    className={`${(task.id === currentMilestone) ? 'bg-gray-800' : 'bg-gray-600'} hover:bg-black p-4 rounded-full focus:bg-green text-white`}
-                    onClick={() => {
-                      setCurrentMilestone(task.id);
-                    }}
-                  >
-                    {task.id}
-                  </button>
-                </div>
-              );
-            }
-          })}
-        </div>
-      </div>
-    );
-  }
 
   const [roadmap, setRoadmap] = useState({
     'name': 'Lorem Ipsum Roadmap',
@@ -252,11 +83,11 @@ function View() {
     <div>
       <div className='flex h-screen bg-white overflow-y-scroll'>
         <div className="container w-5/6 flex-col m-auto py-8 space-y-6">
-          <RoadmapDetails />
-          <RoadmapDisplay roadmap={roadmap} />
-          <MilestoneDetails milestone={currentMilestone} />
+          <RoadmapDetails roadmap={roadmap} detailToggle={detailToggle} />
+          <RoadmapDisplay roadmap={roadmap} setCurrent={setCurrentMilestone} />
+          <MilestoneDetails roadmap={roadmap} current={currentMilestone} />
           <div className="flex flex-col space-y-2">
-            <button onClick={()=>console.log("save!")} className="bg-main-blue grow text-white px-4 py-2 font-semilight rounded-full text-sm font-bold" type="button">
+            <button onClick={() => console.log("save!")} className="bg-main-blue grow text-white px-4 py-2 font-semilight rounded-full text-sm font-bold" type="button">
               Save Changes
             </button>
           </div>
@@ -266,4 +97,145 @@ function View() {
   )
 }
 
-export default View
+function RoadmapDetails({ roadmap, detailToggle }) {
+  return (
+    <>
+      <div className='flex justify-between items-center'>
+        <div className='text-3xl font-extrabold'>
+          {roadmap.name}
+        </div>
+        <div className="flex space-x-1">
+          <button onClick={() => console.log("like!")} className="bg-sub-blue grow text-white px-4 py-2 font-semilight rounded-full text-sm font-bold" type="button">
+            Like
+          </button>
+          <button onClick={() => navigate("/edit", { 'state': { 'roadmap': roadmap } })} className="bg-gray-300 grow text-white px-4 py-2 font-semilight rounded-full text-sm font-bold" type="button">
+            Edit
+          </button>
+          <button onClick={() => setCurrentDetailToggle(!detailToggle)} className="bg-gray-300 grow text-white px-4 py-2 font-semilight rounded-full text-sm font-bold" type="button">
+            i
+          </button>
+        </div>
+      </div>
+      <div className={`${(detailToggle) ? 'visible' : 'hidden'}`}>
+        <div className='flex flex-col bg-gray-100 rounded-2xl shadow-sm p-4 text-sm space-y-2'>
+          <div className='font-bold'>
+            Publicity: {roadmap.publicity ? "Public" : "Private"}
+          </div>
+          <div>
+            {roadmap.description}
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+function RoadmapDisplay({ roadmap, current, setCurrent }) {
+
+  return (
+    <div className='flex bg-gray-100 rounded-2xl shadow-sm p-4 overflow-x-auto'>
+      <div className='flex m-8 space-x-[25px]'>
+        {roadmap.tasks.map(task => {
+          const subtasks = task.subtasks;
+          let currentActiveTask = '1';
+          for (var i = 0; i < subtasks.length; i++) {
+            if (subtasks[i].status === false) {
+              currentActiveTask = task.id;
+              break;
+            }
+          }
+          let nodeShape = "";
+          let nodeColor = "";
+          switch (task.nodeShape) {
+            case "square":
+              nodeShape = "w-[50px] h-[50px] bg-black z-20";
+              break;
+            case "circle":
+              nodeShape = "w-[50px] h-[50px] bg-black rounded-full z-20";
+              break;
+            case "triangle":
+              nodeShape = "w-0 h-0 border-l-[25px] border-r-[25px] border-b-[50px] border-l-transparent border-r-transparent border-b-black z-20";
+              break;
+            default:
+          }
+          switch (task.nodeColor) {
+            case "red":
+              nodeColor = "";
+              break;
+            case "blue":
+              nodeColor = "";
+              break;
+            case "green":
+              nodeColor = "";
+              break;
+            default:
+          }
+          return (
+            <div key={task.id} className='flex items-center relative'>
+
+              <button
+                className={nodeShape}
+                onClick={() => {
+                  setCurrent(task.id);
+                }}
+              />
+              {(task.id !== '1') && (
+                <hr className="w-[75px] h-1 bg-gray-400 border-0 md:my-10 absolute left-[-50px]" />
+              )}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+function MilestoneDetails({ roadmap, current }) {
+  for (let i = 0; i < roadmap.tasks[current - 1].subtasks.length; i++) {
+    console.log(roadmap.tasks[current - 1].subtasks.status);
+  }
+  return (
+    <div className='flex flex-col bg-gray-100 rounded-2xl shadow-sm space-y-4'>
+      <div className='flex flex-row space-x-4 justify-between'>
+        <div className='flex flex-col space-y-2 w-1/2 p-4'>
+          <div className='font-bold'>
+            {roadmap.tasks[current - 1].name}
+          </div>
+          <div className='text-sm'>
+            {roadmap.tasks[current - 1].description}
+          </div>
+          <div className='flex space-x-2'>
+            <div className='grow font-bold text-center p-2'>
+              Start: {roadmap.tasks[current - 1].startDate}
+            </div>
+            <div className='grow font-bold text-center p-2'>
+              Due: {roadmap.tasks[current - 1].dueDate}
+            </div>
+          </div>
+        </div>
+        <div className='flex flex-col space-y-2 w-1/2 p-4 bg-gray-50 rounded-r'>
+          <div className='flex flex-col justify-center space-y-2 text-sm'>
+            {roadmap.tasks[current - 1].subtasks.map(subtask => {
+              return (
+                <label key={subtask.id}>
+                  <input
+                    type="checkbox"
+                    className="w-4 h-4 mr-2 bg-gray-100 border-gray-300 rounded"
+                    defaultChecked={subtask.status ? true : false}
+                    onChange={() => {
+                      let res = { ...roadmap };
+                      res.tasks[current - 1].subtasks[subtask.id - 1].status = !subtask.status;
+                      setRoadmap(res);
+                    }
+                    }
+                  />
+                  {subtask.detail}
+                </label>
+              )
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
