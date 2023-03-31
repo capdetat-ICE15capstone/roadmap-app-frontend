c8b01b6cdfd777d661494e3d76f94c6ee36da59f
Hambaiki <92420824+Hambaiki@users.noreply.github.com>
2023-03-31 21:57:32 +0700

    Implementing conditional rendering for owner vs. viewer (ongoing)
    Added conditional rendering and various functions to support and respond to different conditions.

--- src/pages/View.jsx
+++ src/pages/View.jsx
@@ -8,8 +8,7 @@ import { ReactComponent as Triangle } from "../assets/shapes/Triangle.svg"
 export default function View() {
   const navigate = useNavigate();
 
-  const [currentMilestone, setCurrentMilestone] = useState('1');
-  const [currentDetail, setCurrentDetail] = useState("");
+  const [current, setCurrent] = useState('1');
   const [detailToggle, setCurrentDetailToggle] = useState(true);
 
   const [roadmap, setRoadmap] = useState({
@@ -28,7 +27,7 @@ export default function View() {
         'subtasks': [
           {
             'id': '1',
-            'detail': 'Morbi facilisis finibus lacus quis aliquam.',
+            'detail': 'Morbi Uno finibus lacus quis aliquam.',
             'status': true,
           },
           {
@@ -50,7 +49,7 @@ export default function View() {
           {
             'id': '1',
             'detail': 'Morbi facilisis finibus lacus quis aliquam.',
-            'status': false,
+            'status': true,
           },
           {
             'id': '2',
@@ -70,7 +69,7 @@ export default function View() {
         'subtasks': [
           {
             'id': '1',
-            'detail': 'Morbi facilisis finibus lacus quis aliquam.',
+            'detail': 'Morbi Trio finibus lacus quis aliquam.',
             'status': false,
           },
           {
@@ -83,192 +82,181 @@ export default function View() {
     ]
   });
 
-  return (
-    <div>
-      <div className='flex h-screen bg-white overflow-y-scroll'>
-        <div className="container w-5/6 flex-col m-auto py-8 space-y-6">
-          <RoadmapDetails roadmap={roadmap} detailToggle={detailToggle} />
-          <RoadmapDisplay roadmap={roadmap} setCurrent={setCurrentMilestone} />
-          <MilestoneDetails roadmap={roadmap} current={currentMilestone} />
-          <div className="flex flex-col space-y-2">
-            <button onClick={() => console.log("save!")} className="bg-main-blue grow text-white px-4 py-2 font-semilight rounded-full text-sm font-bold" type="button">
-              Save Changes
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
+  function nodeShapeGenerator(nodeShape) {
+    switch (nodeShape) {
+      case "square":
+        return (<Square />);
+      case "circle":
+        return (<Circle />);
+      case "triangle":
+        return (<Triangle />);
+      default:
+        return (<Circle />);
+    }
+  }
 
-function RoadmapDetails({ roadmap, detailToggle }) {
+  useEffect(() => {
+    for (var i = 0; i < roadmap.tasks.length; i++) {
+      for (var j = 0; j < roadmap.tasks[i].subtasks.length; j++) {
+        if (roadmap.tasks[i].subtasks[j].status === false) {
+          console.log("current task " + roadmap.tasks[i].id);
+          setCurrent(roadmap.tasks[i].id);
+          return;
+        }
+      }
+    }
+  }, []);
+
   return (
     <>
-      <div className='flex justify-between items-center'>
-        <div className='text-3xl font-extrabold'>
-          {roadmap.name}
-        </div>
-        <div className="flex space-x-1">
-          <button onClick={() => console.log("like!")} className="bg-sub-blue grow text-white px-4 py-2 font-semilight rounded-full text-sm font-bold" type="button">
-            Like
-          </button>
-          <button onClick={() => navigate("/edit", { 'state': { 'roadmap': roadmap } })} className="bg-gray-300 grow text-white px-4 py-2 font-semilight rounded-full text-sm font-bold" type="button">
-            Edit
-          </button>
-          <button onClick={() => setCurrentDetailToggle(!detailToggle)} className="bg-gray-300 grow text-white px-4 py-2 font-semilight rounded-full text-sm font-bold" type="button">
-            i
-          </button>
-        </div>
-      </div>
-      <div className={`${(detailToggle) ? 'visible' : 'hidden'}`}>
-        <div className='flex flex-col bg-gray-100 rounded-2xl shadow-sm p-4 text-sm space-y-2'>
-          <div className='font-bold'>
-            Publicity: {roadmap.publicity ? "Public" : "Private"}
+      <div className='flex h-screen bg-white overflow-y-scroll'>
+        <div className="container w-5/6 flex-col m-auto py-8 space-y-6">
+          <div className='flex justify-between items-center'>
+            <div className='text-3xl font-extrabold'>
+              {roadmap.name}
+            </div>
+            {true && (
+              <div className="flex space-x-1">
+                <button onClick={() => console.log("clone!")} className="bg-sub-blue grow text-white px-4 py-2 font-semilight rounded-full text-sm font-bold" type="button">
+                  Clone
+                </button>
+                <button onClick={() => navigate("/edit", { 'state': { 'roadmap': roadmap } })} className="bg-gray-300 grow text-white px-4 py-2 font-semilight rounded-full text-sm font-bold" type="button">
+                  Edit
+                </button>
+                <button onClick={() => setCurrentDetailToggle(!detailToggle)} className="bg-gray-300 grow text-white px-4 py-2 font-semilight rounded-full text-sm font-bold" type="button">
+                  i
+                </button>
+              </div>
+            )}
+            {false && (
+              <div className="flex space-x-1">
+                <button onClick={() => console.log("clone!")} className="bg-sub-blue grow text-white px-4 py-2 font-semilight rounded-full text-sm font-bold" type="button">
+                  Clone
+                </button>
+                <button onClick={() => navigate("/edit", { 'state': { 'roadmap': roadmap } })} className="bg-gray-300 grow text-white px-4 py-2 font-semilight rounded-full text-sm font-bold" type="button">
+                  Like
+                </button>
+                <button onClick={() => setCurrentDetailToggle(!detailToggle)} className="bg-gray-300 grow text-white px-4 py-2 font-semilight rounded-full text-sm font-bold" type="button">
+                  i
+                </button>
+              </div>
+            )}
           </div>
-          <div>
-            {roadmap.description}
+          <div className={`${(detailToggle) ? 'visible' : 'hidden'}`}>
+            <div className='flex flex-col bg-gray-100 rounded-2xl shadow-sm p-4 text-sm space-y-2'>
+              <div className='font-bold'>
+                Publicity: {roadmap.publicity ? "Public" : "Private"}
+              </div>
+              <div>
+                {roadmap.description}
+              </div>
+            </div>
+          </div>
+          <div className='flex flex-col bg-gray-100 rounded-2xl shadow-sm p-4 overflow-x-auto'>
+            {true && (
+              <div className='flex m-8 space-x-[25px]'>
+                {roadmap.tasks.map((task, index) => {
+                  const zIndex = 10 - index;
+                  return (
+                    <div key={task.id} className="relative" style={{ zIndex }}>
+                      <div className="absolute top-1/2 -left-1/4 transform -translate-x-1/2 -translate-y-3/4 -z-10">
+                        {(task.id !== '1') && <hr className="w-[75px] h-1 bg-sub-blue border-0" />}
+                      </div>
+                      {nodeShapeGenerator(task.nodeShape)}
+                    </div>
+                  );
+                })}
+              </div>
+            )}
+            {false && (
+              <div className='flex m-8 space-x-[25px]'>
+                {roadmap.tasks.map((task, index) => {
+                  const zIndex = 10 - index;
+                  return (
+                    <div key={task.id} className="relative" style={{ zIndex }}>
+                      <div className="absolute top-1/2 -left-1/4 transform -translate-x-1/2 -translate-y-3/4 -z-10">
+                        {(task.id !== '1') && <hr className="w-[75px] h-1 bg-sub-blue border-0" />}
+                      </div>
+                      <button onClick={() => setCurrent(task.id)}>
+                        {nodeShapeGenerator(task.nodeShape)}
+                      </button>
+                    </div>
+                  );
+                })}
+              </div>
+            )}
+            <div className='flex justify-between'>
+              <div className='flex text-sm'>
+                0 Views
+              </div>
+              <div className='flex text-sm'>
+                Latest update: 000000
+              </div>
+            </div>
+          </div>
+          <div className='flex flex-col bg-gray-100 rounded-2xl shadow-sm space-y-4'>
+            <div className='flex flex-row space-x-4 justify-between'>
+              <div className='flex flex-col space-y-2 w-1/2 p-4'>
+                <div className='font-bold'>
+                  {roadmap.tasks[current - 1].name}
+                </div>
+                <div className='text-sm'>
+                  {roadmap.tasks[current - 1].description}
+                </div>
+                <div className='flex space-x-2'>
+                  <div className='grow font-bold text-center p-2'>
+                    Start: {roadmap.tasks[current - 1].startDate}
+                  </div>
+                  <div className='grow font-bold text-center p-2'>
+                    Due: {roadmap.tasks[current - 1].dueDate}
+                  </div>
+                </div>
+              </div>
+              {true && (
+                <div className='flex flex-col space-y-2 w-1/2 p-4 bg-gray-50 rounded-r justify-between'>
+                  <div className='flex flex-col justify-center space-y-2 text-sm'>
+                    {roadmap.tasks[current - 1].subtasks.map(subtask => {
+                      return (
+                        <label key={subtask.id}>
+                          <input
+                            type="checkbox"
+                            className="w-4 h-4 mr-2 bg-gray-100 border-gray-300 rounded"
+                            checked={subtask.status}
+                            onChange={() => {
+                              let res = { ...roadmap };
+                              res.tasks[current - 1].subtasks[subtask.id - 1].status = !subtask.status;
+                              setRoadmap(res);
+                            }}
+                          />
+                          {subtask.detail}
+                        </label>
+                      )
+                    })}
+                  </div>
+                  <div className='flex'>
+                    <button onClick={() => console.log("save!")} className="bg-sub-blue grow text-white px-4 py-2 font-semilight rounded-full text-sm font-bold" type="button">
+                      Save
+                    </button>
+                  </div>
+                </div>
+              )}
+              {false && (
+                <div className='flex flex-col space-y-2 w-1/2 p-4 bg-gray-50 rounded-r'>
+                  <div className='flex flex-col justify-center space-y-2 text-sm'>
+                    {roadmap.tasks[current - 1].subtasks.map(subtask => {
+                      return (
+                        <label key={subtask.id}>
+                          {subtask.detail}
+                        </label>
+                      )
+                    })}
+                  </div>
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
     </>
-  )
-}
-
-function RoadmapDisplay({ roadmap, current, setCurrent }) {
-
-  return (
-    <div className='flex flex-col bg-gray-100 rounded-2xl shadow-sm p-4 overflow-x-auto'>
-      <div className='flex m-8 space-x-[25px]'>
-        {roadmap.tasks.map(task => {
-          const subtasks = task.subtasks;
-          let currentActiveTask = '1';
-          for (var i = 0; i < subtasks.length; i++) {
-            if (subtasks[i].status === false) {
-              currentActiveTask = task.id;
-              break;
-            }
-          }
-          let nodeShape = "";
-          let nodeColor = "";
-          switch (task.nodeShape) {
-            case "square":
-              nodeShape = "w-[50px] h-[50px] bg-black z-20";
-              break;
-            case "circle":
-              nodeShape = "w-[50px] h-[50px] bg-black rounded-full z-20";
-              break;
-            case "triangle":
-              nodeShape = "w-0 h-0 border-l-[25px] border-r-[25px] border-b-[50px] border-l-transparent border-r-transparent border-b-black z-20";
-              break;
-            default:
-          }
-          switch (task.nodeColor) {
-            case "red":
-              nodeColor = "bg-red";
-              break;
-            case "blue":
-              nodeColor = "bg-red";
-              break;
-            case "green":
-              nodeColor = "bg-red";
-              break;
-            default:
-          }
-          return (
-            <div key={task.id} className='flex flex-col items-center'>
-              {/* <div className='text-sm font-bold text-nav-blue mb-2'>
-                50 xp
-              </div> */}
-              <div className="relative">
-                <button
-                  onClick={() => {
-                    setCurrent(task.id);
-                  }}
-                >
-                  {nodeShapeGenerator(task.nodeShape)}
-                </button>
-                <div className="absolute bottom-0 left-0">
-                  <Circle />
-                  {(task.id !== '1') && (
-                    <hr className="w-[75px] h-1 bg-gray-400 border-0 z-10" />
-                  )}
-                </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-      <div className='flex justify-between'>
-        <div className='flex text-sm'>
-          0 Views
-        </div>
-        <div className='flex text-sm'>
-          Latest update: 000000
-        </div>
-      </div>
-    </div>
-  );
-}
-
-function nodeShapeGenerator({ nodeShape }) {
-  switch (nodeShape) {
-    case "square":
-      return (<Square />);
-    case "circle":
-      return (<Circle />);
-    case "triangle":
-      return (<Triangle />);
-    default:
-      return (<Circle />);
-  }
-}
-
-function MilestoneDetails({ roadmap, current }) {
-  for (let i = 0; i < roadmap.tasks[current - 1].subtasks.length; i++) {
-    console.log(roadmap.tasks[current - 1].subtasks.status);
-  }
-  return (
-    <div className='flex flex-col bg-gray-100 rounded-2xl shadow-sm space-y-4'>
-      <div className='flex flex-row space-x-4 justify-between'>
-        <div className='flex flex-col space-y-2 w-1/2 p-4'>
-          <div className='font-bold'>
-            {roadmap.tasks[current - 1].name}
-          </div>
-          <div className='text-sm'>
-            {roadmap.tasks[current - 1].description}
-          </div>
-          <div className='flex space-x-2'>
-            <div className='grow font-bold text-center p-2'>
-              Start: {roadmap.tasks[current - 1].startDate}
-            </div>
-            <div className='grow font-bold text-center p-2'>
-              Due: {roadmap.tasks[current - 1].dueDate}
-            </div>
-          </div>
-        </div>
-        <div className='flex flex-col space-y-2 w-1/2 p-4 bg-gray-50 rounded-r'>
-          <div className='flex flex-col justify-center space-y-2 text-sm'>
-            {roadmap.tasks[current - 1].subtasks.map(subtask => {
-              return (
-                <label key={subtask.id}>
-                  <input
-                    type="checkbox"
-                    className="w-4 h-4 mr-2 bg-gray-100 border-gray-300 rounded"
-                    defaultChecked={subtask.status ? true : false}
-                    onChange={() => {
-                      let res = { ...roadmap };
-                      res.tasks[current - 1].subtasks[subtask.id - 1].status = !subtask.status;
-                      setRoadmap(res);
-                    }
-                    }
-                  />
-                  {subtask.detail}
-                </label>
-              )
-            })}
-          </div>
-        </div>
-      </div>
-    </div>
   )
 }
