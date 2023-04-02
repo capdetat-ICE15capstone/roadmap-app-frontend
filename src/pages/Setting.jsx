223d98e3cf3b18672f3028b731cd66e077536407
SupakornSJB <limit4break@gmail.com>
2023-04-02 15:24:09 +0700

    work on Notification and PWA
    - add manifest file for PWA
    - add push event listener for notification
    - add logic for making sub obj
    - add logic for sending sub obj to server
    - add logic for unsubscribing push service from push manager

--- src/pages/Setting.jsx
+++ src/pages/Setting.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const publicVAPIDKey =
   "BPdmC1qk09Z30BA96nNmddDNEr6M9fjnBaG15Ezpe_f-J7qQIEAUEbWTSFuQDxzDmDMfwB-al5xANDwYfcwt8P4";
+
+const route = "http://localhost:8888/subscribe";
 
 function urlBase64ToUint8Array(base64String) {
   var padding = "=".repeat((4 - (base64String.length % 4)) % 4);
@@ -17,43 +20,99 @@ function urlBase64ToUint8Array(base64String) {
 }
 
 const Setting = () => {
-  const [notiOn, setNotiOn] = useState(true);
+  const [notiOn, setNotiOn] = useState(false);
 
   const handleNotiSubscription = () => {
-    console.log("function handlenotisub")
+    console.log("function handlenotisub");
     if (notiOn === true) return;
 
-    navigator.serviceWorker.ready.then((serviceWorkerRegistration) => {
-      const options = {
-        userVisibleOnly: true,
-        applicationServerKey: urlBase64ToUint8Array(publicVAPIDKey),
-      };
-
-      serviceWorkerRegistration.pushManager
-        .subscribe(options)
-        .then(
-          (pushSubscription) => {
-            console.log(pushSubscription.endpoint);
-          },
-          (error) => {
-            console.log(error);
+    try {
+      // Ask for notification permission
+      if (!("Notification" in window)) {
+        alert("This browser does not support desktop notification");
+        // TELL THE USER THEY DONT HAVE NOTIFICATION AVAILABLE
+        throw new Error("Notification Not supported");
+      } else if (Notification.permission === "granted") {
+        console.log("Permission granted");
+      } else if (Notification.permission !== "denied") {
+        Notification.requestPermission().then((permission) => {
+          if (permission === "granted") {
+            console.log("Permission granted");
+          } else {
+            console.error("Notification Permission: Denied");
+            // TELL THE USER NOTIFICATION PERMISSION IS DENIED
+            throw new Error("Notification Permission: Denied");
           }
-        )
-        .then(() => setNotiOn(true))
-        .catch(() => console.log("Subscription error"))
-    }).catch(() => {
-        console.log("service worker not ready")
-    });
+        });
+      } else {
+        console.error("Notification Permission: Denied");
+        // TELL THE USER NOTIFICATION PERMISSION IS DENIED
+        throw new Error("Notification Permission: Denied");
+      }
+
+      navigator.serviceWorker.ready
+        .then((serviceWorkerRegistration) => {
+          const options = {
+            userVisibleOnly: true,
+            applicationServerKey: urlBase64ToUint8Array(publicVAPIDKey),
+          };
+
+          serviceWorkerRegistration.pushManager
+            .subscribe(options)
+            .then(
+              (pushSubscription) => {
+                console.log(pushSubscription);
+                axios.post(route, pushSubscription)
+                    .catch(handleNotiUnsubscription)
+              },
+              (error) => {
+                console.error(error);
+              }
+            )
+            .then(() => {
+              setNotiOn(true);
+              console.log("registration complete");
+            })
+            .catch((error) => {
+              console.error("Subscription error");
+              console.error(error);
+            });
+        })
+        .catch((error) => {
+          console.error("service worker not ready");
+          console.error(error);
+        });
+    } catch (error) {
+      console.error("setup error");
+      console.error(error);
+    }
   };
 
   const handleNotiUnsubscription = () => {
     console.log("unsub");
+    navigator.serviceWorker.ready.then((reg) => {
+      reg.pushManager.getSubscription().then((subscription) => {
+        // Tell the server unsub
+        console.log(subscription);
+        subscription
+          .unsubscribe()
+          .then((successful) => {
+            // You've successfully unsubscribed
+            console.log(successful);
+            setNotiOn(false);
+          })
+          .catch((e) => {
+            // Unsubscribing failed
+            console.error(e);
+          });
+      });
+    });
   };
 
   return (
     <button
       className="bg-red-400 w-20 h-10"
-      onClick={notiOn ? handleNotiSubscription : handleNotiUnsubscription}
+      onClick={notiOn ? handleNotiUnsubscription : handleNotiSubscription}
     >
       Sub
     </button>
--- src/sw.js
+++ src/sw.js
@@ -1,10 +1,16 @@
 import { precacheAndRoute } from "workbox-precaching";
 
 const myCache = self.__WB_MANIFEST;
+console.log(myCache);
 precacheAndRoute(myCache);
 console.warn("Service worker is now operable");
 
-self.addEventListener("push", () => {
-    // listen for push notification
-    
-})
+self.addEventListener("push", (event) => {
+  // listen for push notification
+  const data = event.data.json();
+  console.log(data);
+  self.registration.showNotification(data.title, {
+    body: data.body,
+    icon: "android-chrome-192x192.png",
+  });
+});
