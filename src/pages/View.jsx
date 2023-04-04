9fb8238af5aa17d27152b2ddf43b7946138002be
Hambaiki <92420824+Hambaiki@users.noreply.github.com>
2023-04-04 19:30:06 +0700

    Some updates to View page

--- src/pages/View.jsx
+++ src/pages/View.jsx
@@ -20,151 +20,153 @@ export default function View() {
   const [detailToggle, setDetailToggle] = useState(true);
   const [saveToggle, setSaveToggle] = useState(false);
   const [nodeViewToggle, setNodeViewToggle] = useState(false);
-  const [updates, setUpdates] = useState([]);
+  const [updates, setUpdates] = useState(false);
   const [currentSubtasks, setCurrentSubtasks] = useState({});
   const [liked, setLiked] = useState(false);
+  const [completeButton, setCompleteButton] = useState(false);
 
-  const [myProfile, setMyProfile] = useState({
-    "profile": {
-      "username": "zack",
-      "is_premium": false,
-      "is_active": true,
-      "points": 0,
-      "profile_picture_id": 0,
-      "is_private": false,
-      "exp": 0,
-      "bio": null
-    },
-    "roadmaps": [],
-    "archived_roadmaps": []
-  });
+  // const [myProfile, setMyProfile] = useState({
+  //   "profile": {
+  //     "username": "zack",
+  //     "is_premium": false,
+  //     "is_active": true,
+  //     "points": 0,
+  //     "profile_picture_id": 0,
+  //     "is_private": false,
+  //     "exp": 0,
+  //     "bio": null
+  //   },
+  //   "roadmaps": [],
+  //   "archived_roadmaps": []
+  // });
 
   // When request forking, check if roadmap has more than 16 nodes... if roadmap has more than 16 nodes, check if the user has premium. If not, prompt clone failure.\
   // Also check for roadmaps.length?
 
-  const [roadmap, setRoadmap] = useState(
-    {
-      "title": "Alpha male training",
-      "description": "What is your Buggati color",
-      "roadmap_deadline": "2023-04-03T02:45:24.100000",
-      "is_before_start_time": true,
-      "reminder_time": 0,
-      "is_private": false,
-      "rid": 17,
-      "views_count": 0,
-      "stars_count": 0,
-      "forks_count": 0,
-      "edited_at": "2023-04-03T03:06:47",
-      "owner_id": 19,
-      "creator_id": 19,
-      "owner_name": null,
-      "creator_name": null,
-      "owner_profile_picture_id": null,
-      "creator_profile_picture_id": null,
-      "next_task": {
-        "title": "Convert to Islam",
-        "description": "Allah",
-        "start_time": "2023-04-03T03:01:42.600000",
-        "deadline": "2023-04-03T03:01:42.600000",
-        "shape": "triangle",
-        "color": "blue",
-        "tid": 52,
-        "is_done": false,
-        "subtasks": []
-      },
-      "tasks_name": [
-        "Convert to Islam"
-      ],
-      "shapes": [
-        "triangle"
-      ],
-      "colors": [
-        "blue"
-      ],
-      "tags": [],
-      "task_relation": [
-        52
-      ],
-      "archive_date": null,
-      "exp": 0
-    }
-  );
+  // const [roadmap, setRoadmap] = useState(
+  //   {
+  //     "title": "Alpha male training",
+  //     "description": "What is your Buggati color",
+  //     "roadmap_deadline": "2023-04-03T02:45:24.100000",
+  //     "is_before_start_time": true,
+  //     "reminder_time": 0,
+  //     "is_private": false,
+  //     "rid": 17,
+  //     "views_count": 0,
+  //     "stars_count": 0,
+  //     "forks_count": 0,
+  //     "edited_at": "2023-04-03T03:06:47",
+  //     "owner_id": 19,
+  //     "creator_id": 19,
+  //     "owner_name": null,
+  //     "creator_name": null,
+  //     "owner_profile_picture_id": null,
+  //     "creator_profile_picture_id": null,
+  //     "next_task": {
+  //       "title": "Convert to Islam",
+  //       "description": "Allah",
+  //       "start_time": "2023-04-03T03:01:42.600000",
+  //       "deadline": "2023-04-03T03:01:42.600000",
+  //       "shape": "triangle",
+  //       "color": "blue",
+  //       "tid": 52,
+  //       "is_done": false,
+  //       "subtasks": []
+  //     },
+  //     "tasks_name": [
+  //       "Convert to Islam"
+  //     ],
+  //     "shapes": [
+  //       "triangle"
+  //     ],
+  //     "colors": [
+  //       "blue"
+  //     ],
+  //     "tags": [],
+  //     "task_relation": [
+  //       52
+  //     ],
+  //     "archive_date": null,
+  //     "exp": 0
+  //   }
+  // );
 
-  // const [roadmap, setRoadmap] = useState({
-  //   'name': 'Lorem Ipsum Roadmap',
-  //   'description': 'Morbi facilisis finibus lacus quis aliquam. Vestibulum turpis nibh, imperdiet non gravida quis, pretium vitae est. Phasellus in sollicitudin quam, id lacinia nisl.',
-  //   'publicity': true,
-  //   'owner-id': 123456,
-  //   'like': 99,
-  //   'tasks': [
-  //     {
-  //       'id': '1',
-  //       'name': 'First Task',
-  //       'description': 'Task descriptions. This is the task for the first milestone.',
-  //       'startDate': '20200110',
-  //       'dueDate': '20200115',
-  //       'nodeColor': '#7DC5E3',
-  //       'nodeShape': 'circle',
-  //       'subtasks': [
-  //         {
-  //           'id': '1',
-  //           'detail': 'Morbi Uno finibus lacus quis aliquam.',
-  //           'status': true,
-  //         },
-  //         {
-  //           'id': '2',
-  //           'detail': 'Vestibulum turpis nibh.',
-  //           'status': true,
-  //         }
-  //       ]
-  //     },
-  //     {
-  //       'id': '2',
-  //       'name': 'Second Task',
-  //       'description': 'Task descriptions. This is the task for the second milestone.',
-  //       'startDate': '20200115',
-  //       'dueDate': '20200120',
-  //       'nodeColor': '#E4688D',
-  //       'nodeShape': 'square',
-  //       'subtasks': [
-  //         {
-  //           'id': '1',
-  //           'detail': 'Morbi facilisis finibus lacus quis aliquam.',
-  //           'status': true,
-  //         },
-  //         {
-  //           'id': '2',
-  //           'detail': 'Vestibulum turpis nibh.',
-  //           'status': false,
-  //         }
-  //       ]
-  //     },
-  //     {
-  //       'id': '3',
-  //       'name': 'Third Task',
-  //       'description': 'Task descriptions. This is the task for the third milestone.',
-  //       'startDate': '20200120',
-  //       'dueDate': '20200121',
-  //       'nodeColor': '#F3B21A',
-  //       'nodeShape': 'triangle',
-  //       'subtasks': [
-  //         {
-  //           'id': '1',
-  //           'detail': 'Morbi Trio finibus lacus quis aliquam.',
-  //           'status': false,
-  //         },
-  //         {
-  //           'id': '2',
-  //           'detail': 'Vestibulum turpis nibh.',
-  //           'status': false,
-  //         }
-  //       ]
-  //     }
-  //   ]
-  // });
+  const [roadmap, setRoadmap] = useState({
+    'name': 'Lorem Ipsum Roadmap',
+    'description': 'Morbi facilisis finibus lacus quis aliquam. Vestibulum turpis nibh, imperdiet non gravida quis, pretium vitae est. Phasellus in sollicitudin quam, id lacinia nisl.',
+    'publicity': true,
+    'owner-id': 123456,
+    'is-active': true,
+    'like': 99,
+    'tasks': [
+      {
+        'id': '1',
+        'name': 'First Task',
+        'description': 'Task descriptions. This is the task for the first milestone.',
+        'startDate': '20200110',
+        'dueDate': '20200115',
+        'nodeColor': '#7DC5E3',
+        'nodeShape': 'circle',
+        'subtasks': [
+          {
+            'id': '1',
+            'detail': 'Morbi Uno finibus lacus quis aliquam.',
+            'status': true,
+          },
+          {
+            'id': '2',
+            'detail': 'Vestibulum turpis nibh.',
+            'status': true,
+          }
+        ]
+      },
+      {
+        'id': '2',
+        'name': 'Second Task',
+        'description': 'Task descriptions. This is the task for the second milestone.',
+        'startDate': '20200115',
+        'dueDate': '20200120',
+        'nodeColor': '#E4688D',
+        'nodeShape': 'square',
+        'subtasks': [
+          {
+            'id': '1',
+            'detail': 'Morbi facilisis finibus lacus quis aliquam.',
+            'status': true,
+          },
+          {
+            'id': '2',
+            'detail': 'Vestibulum turpis nibh.',
+            'status': false,
+          }
+        ]
+      },
+      {
+        'id': '3',
+        'name': 'Third Task',
+        'description': 'Task descriptions. This is the task for the third milestone.',
+        'startDate': '20200120',
+        'dueDate': '20200121',
+        'nodeColor': '#F3B21A',
+        'nodeShape': 'triangle',
+        'subtasks': [
+          {
+            'id': '1',
+            'detail': 'Morbi Trio finibus lacus quis aliquam.',
+            'status': false,
+          },
+          {
+            'id': '2',
+            'detail': 'Vestibulum turpis nibh.',
+            'status': false,
+          }
+        ]
+      }
+    ]
+  });
 
   function likeRoadmap() {
-    let res = {...roadmap};
+    let res = { ...roadmap };
     if (liked) {
       res.like -= 1;
       setLiked(false);
@@ -175,9 +177,16 @@ export default function View() {
     setRoadmap(res);
   }
 
+  function completeRoadmap() {
+    let res = { ...roadmap };
+    res['is-active'] = false;
+    setRoadmap(res);
+    setCompleteButton(false);
+  }
+
   function nodeShapeGenerator(nodeShape, nodeColor, node_id) {
     let currentNodeColor = "";
-    if (node_id !== null) {
+    if (node_id !== null && roadmap['is-active'] === true) {
       if (node_id < current) {
         currentNodeColor = "#707070";
       } else if (node_id > current) {
@@ -185,6 +194,8 @@ export default function View() {
       } else {
         currentNodeColor = nodeColor;
       }
+    } else {
+      currentNodeColor = nodeColor;
     }
     switch (nodeShape) {
       case "square":
@@ -228,6 +239,9 @@ export default function View() {
         }
       }
       setCurrent('-1');
+      if (roadmap['is-active'] === true) {
+        setCompleteButton(true);
+      }
     }
   }, [roadmap]);
 
@@ -444,69 +458,81 @@ export default function View() {
               )}
             </div>
           )}
-      </div>
-      {saveToggle && (
-        <div className="fixed inset-0 z-50">
-          <div className="absolute inset-0 z-10 flex items-center justify-center">
-            <div className="flex flex-col rounded-2xl p-4 bg-white space-y-4 shadow-lg">
-              <div>
-                Are you sure you want to save?
+          {completeButton && (
+            <div className='flex flex-col justify-center bg-gray-100 rounded-2xl shadow-sm space-y-4 p-4'>
+              <div className='flex justify-center font-bold'>
+                Congratulations! You have complete all the milestones in your roadmap. Click on the "complete" button to archive your roadmap.
               </div>
-              <div className='flex space-x-2'>
-                <button onClick={() => setSaveToggle(!saveToggle)} className="bg-gray-400 w-1/2 text-white px-4 py-2 rounded-full text-sm font-bold" type="button">
-                  Cancel
-                </button>
-                <button onClick={() => saveRoadmap()} className="bg-sub-blue w-1/2 text-white px-4 py-2 rounded-full text-sm font-bold" type="button">
-                  Save
-                </button>
+              <div className='flex justify-center'>
+                  <button onClick={() => completeRoadmap()} className="bg-sub-blue w-1/4 text-white px-4 py-2 rounded-full text-sm font-bold" type="button">
+                    Complete
+                  </button>
               </div>
             </div>
-          </div>
-          <div className="bg-black opacity-50 w-full h-full"></div>
+          )}
         </div>
-      )}
-      {nodeViewToggle && (
-        <div className="fixed inset-0 z-50">
-          <div className="absolute inset-0 z-10 flex items-center justify-center">
-            <div className="flex flex-col rounded-2xl p-4 bg-white space-y-4 shadow-lg">
-              <div className='flex flex-row'>
-                <div className='flex flex-col space-y-2 w-1/2 p-4'>
-                  <div className='font-bold'>
-                    {roadmap.tasks[currentViewNode - 1].name}
-                  </div>
-                  <div className='text-sm'>
-                    {roadmap.tasks[currentViewNode - 1].description}
-                  </div>
-                  <div className='flex space-x-2'>
-                    <div className='grow font-bold text-center p-2'>
-                      Start: {roadmap.tasks[currentViewNode - 1].startDate}
-                    </div>
-                    <div className='grow font-bold text-center p-2'>
-                      Due: {roadmap.tasks[currentViewNode - 1].dueDate}
-                    </div>
-                  </div>
+        {saveToggle && (
+          <div className="fixed inset-0 z-50">
+            <div className="absolute inset-0 z-10 flex items-center justify-center">
+              <div className="flex flex-col rounded-2xl p-4 bg-white space-y-4 shadow-lg">
+                <div>
+                  Are you sure you want to save?
                 </div>
-                <div className='flex flex-col w-1/2 p-4 justify-center space-y-2 text-sm'>
-                  {roadmap.tasks[currentViewNode - 1].subtasks.map(subtask => {
-                    return (
-                      <label key={subtask.id}>
-                        {subtask.detail}
-                      </label>
-                    )
-                  })}
+                <div className='flex space-x-2'>
+                  <button onClick={() => setSaveToggle(!saveToggle)} className="bg-gray-400 w-1/2 text-white px-4 py-2 rounded-full text-sm font-bold" type="button">
+                    Cancel
+                  </button>
+                  <button onClick={() => saveRoadmap()} className="bg-sub-blue w-1/2 text-white px-4 py-2 rounded-full text-sm font-bold" type="button">
+                    Save
+                  </button>
                 </div>
-              </div>
-              <div className='flex space-x-2'>
-                <button onClick={() => setNodeViewToggle(!nodeViewToggle)} className="bg-gray-400 w-full text-white px-4 py-2 rounded-full text-sm font-bold" type="button">
-                  Close
-                </button>
               </div>
             </div>
+            <div className="bg-black opacity-50 w-full h-full"></div>
           </div>
-          <div className="bg-black opacity-50 w-full h-full"></div>
-        </div>
-      )}
-    </div >
+        )}
+        {nodeViewToggle && (
+          <div className="fixed inset-0 z-50">
+            <div className="absolute inset-0 z-10 flex items-center justify-center">
+              <div className="flex flex-col rounded-2xl p-4 bg-white space-y-4 shadow-lg">
+                <div className='flex flex-row'>
+                  <div className='flex flex-col space-y-2 w-1/2 p-4'>
+                    <div className='font-bold'>
+                      {roadmap.tasks[currentViewNode - 1].name}
+                    </div>
+                    <div className='text-sm'>
+                      {roadmap.tasks[currentViewNode - 1].description}
+                    </div>
+                    <div className='flex space-x-2'>
+                      <div className='grow font-bold text-center p-2'>
+                        Start: {roadmap.tasks[currentViewNode - 1].startDate}
+                      </div>
+                      <div className='grow font-bold text-center p-2'>
+                        Due: {roadmap.tasks[currentViewNode - 1].dueDate}
+                      </div>
+                    </div>
+                  </div>
+                  <div className='flex flex-col w-1/2 p-4 justify-center space-y-2 text-sm'>
+                    {roadmap.tasks[currentViewNode - 1].subtasks.map(subtask => {
+                      return (
+                        <label key={subtask.id}>
+                          {subtask.detail}
+                        </label>
+                      )
+                    })}
+                  </div>
+                </div>
+                <div className='flex space-x-2'>
+                  <button onClick={() => setNodeViewToggle(!nodeViewToggle)} className="bg-gray-400 w-full text-white px-4 py-2 rounded-full text-sm font-bold" type="button">
+                    Close
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="bg-black opacity-50 w-full h-full"></div>
+          </div>
+        )}
+      </div >
       <button onClick={() => setIsOwner(!isOwner)} className="bg-nav-blue text-white px-4 py-2 font-semilight text-sm font-bold" type="button">
         CHANGE VIEW MODE (FOR DEVELOPMENT)
       </button>
