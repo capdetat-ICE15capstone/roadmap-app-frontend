68bfa730d6490557d24c5c5c0fe2105f87a3edc4
ThanapatTr <6338098421@student.chula.ac.th>
2023-04-04 21:12:11 +0700

    change feed to only just for reccommend roadmaps and add searchPage

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,23 +1,24 @@
 import React, { useState, useEffect, useRef } from 'react';
 import Roadmap from "../components/Roadmap";
 import SearchBar from "../components/SearchBar";
+import { useNavigate } from "react-router-dom";
 
 const Feed = () => {
 
   const [search, setSearch] = useState("");
   const [roadmapArray, setRoadmapArray] = useState([]);
-  const [page, setPage] = useState(1);
-  const containerRef = useRef(null);
   const isMountedRef = useRef(false);
   const [isFetching, setIsFetching] = useState(false);
+  const navigate = useNavigate();
 
   const handleSubmit = (event) => {
     event.preventDefault();
     setRoadmapArray([]);
-    setPage(1);
-    fetchData();
+    //fetchData();
     setSearch(document.getElementById("InputSearch").value);
-    console.log("search: " + document.getElementById("InputSearch").value + "Page: " + page);
+    const searchValue = document.getElementById("InputSearch").value;
+    navigate('/search', { state: { userSearch: searchValue } });
+    console.log("search: " + document.getElementById("InputSearch").value);
   };
 
   //fetch roadmap data from API
@@ -26,7 +27,7 @@ const Feed = () => {
     setIsFetching(true); // set isFetching to true to indicate a fetch is starting
     try {
       console.log("fetch")
-      const response = await fetch(`http://localhost:3000/multipleRoadmaps`); 
+      const response = await fetch(`http://localhost:3000/multipleRoadmaps`);
       const data = await response.json();
       let newArray = [];
       data.forEach((data) => {
@@ -47,16 +48,7 @@ const Feed = () => {
       return;
     }
     fetchData();
-  }, [page]);
-
-  // set page +1 when scrolled to the bottom
-  function handleScroll() {
-    const container = containerRef.current;
-    if (container.scrollTop + container.clientHeight >= container.scrollHeight) {
-      setPage(prevPage => prevPage + 1);
-      console.log('Scrolled to bottom');
-    }
-  }
+  }, []);
 
   return (
     <>
@@ -73,17 +65,17 @@ const Feed = () => {
         </div>
       </form>
       {/*Search Result*/}
-      <div className="relative flex flex-wrap mx-4 overflow-y-scroll " onScroll={handleScroll} ref={containerRef}>
+      <div className="relative flex flex-wrap mx-4 overflow-y-scroll ">
         {roadmapArray.map((roadmap, index) => (
           <div key={index} className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-2">
             <Roadmap
-              owner_id = {roadmap.owner_id}
-              creator_id = {roadmap.creator_id}
+              owner_id={roadmap.owner_id}
+              creator_id={roadmap.creator_id}
               owner_name={roadmap.owner_name}
               creator_name={roadmap.creator_name}
-              rid = {roadmap.rid}
+              rid={roadmap.rid}
               views_count={roadmap.views_count}
-              stars_count = {roadmap.stars_count}
+              stars_count={roadmap.stars_count}
               forks_count={roadmap.forks_count}
               created_at={roadmap.created_at}
               edited_at={roadmap.edited_at}
@@ -97,4 +89,4 @@ const Feed = () => {
 };
 
 export default Feed;
-//npx json-server --watch json_server_test/db.json
+//                         npx json-server --watch json_server_test/db.json
