e7fde6f1819fce4a411819dea690492b053c3838
wuttikorn.n45 <wuttikorn.n45@gmail.com>
2023-04-09 13:29:44 +0700

    add alert box when no of roadmap reaches 3

--- src/components/RoadmapCreate.jsx
+++ src/components/RoadmapCreate.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { ReactComponent as PlusIcon } from "../assets/roadmap_assets/plus_Icon.svg"
 import { Link } from "react-router-dom";
 
-const RoadmapCreate = () => {
+const RoadmapCreate = ({isLimit, onClick}) => {
     return (
-        <Link to={"/create"} className="relative flex items-center justify-center w-[200px] h-[220px] mr-16 my-8 border-dashed border-2 border-gray-400 rounded-[48px]">
-            <PlusIcon className="absolute w-16 h-16" />
-        </Link>
+        <>
+            {isLimit? 
+            <Link to={"/"} onClick={onClick} className="relative flex items-center justify-center w-[200px] h-[220px] mr-16 my-8 border-dashed border-2 border-gray-400 rounded-[48px]">
+                <PlusIcon className="absolute w-16 h-16" />
+            </Link>:
+            <Link to={"/create"} className="relative flex items-center justify-center w-[200px] h-[220px] mr-16 my-8 border-dashed border-2 border-gray-400 rounded-[48px]">
+                <PlusIcon className="absolute w-16 h-16" />
+            </Link>}
+        </>
     );
 };
 export default RoadmapCreate;
