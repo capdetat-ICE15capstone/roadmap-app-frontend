56a13e80402a698c3dc3fdc1be123d8245d9a009
James <49901378+Mifuu@users.noreply.github.com>
2023-04-11 01:43:59 +0700

    Activity API Done
    Need to place complete quest in places though

--- src/functions/userFunction.jsx
+++ src/functions/userFunction.jsx
@@ -1,6 +1,27 @@
+import { axiosInstance } from "./axiosInstance";
+
 export const isUserPremium = () => {
     // check whether or not the user is premium
     // return true if the user is premium
     // return false if the user is not logged in or the user is not premium
     return true; // PLACEHOLDER PLS DONT LAUGH
+}
+
+export const isUserLoggedIn = async (timeout = 0) => {
+    // check whether user is logged-in
+    /*
+    const route = `/roadmap/count/`
+
+    try {
+      let response = await axiosInstance.get(route, { timeout: timeout });
+      return true;
+    } catch (error) {
+      return false;
+    }
+    */
+
+    if (localStorage.getItem("token") === undefined) {
+      return false;
+    }
+    return true;
 }
