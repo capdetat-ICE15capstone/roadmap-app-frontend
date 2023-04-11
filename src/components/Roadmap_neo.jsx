7045f23f2e938d5cc327a53174011a2eb628ee13
ThanapatTr <6338098421@student.chula.ac.th>
2023-04-12 00:16:22 +0700

    roadmap complete

--- src/components/Roadmap_neo.jsx
+++ src/components/Roadmap_neo.jsx
@@ -26,14 +26,14 @@ const Roadmap = ({ owner_id, creator_id, owner_name, creator_name, rid, views_co
   // change created_at to dd/mm/yy format
   const date = new Date(created_at);
   const day = date.getUTCDate();
-  const month = date.getUTCMonth() + 1; 
+  const month = date.getUTCMonth() + 1;
   const year = date.getUTCFullYear().toString().substr(-2);
   const created_at_format = `${day}/${month}/${year}`;
 
   // change edited_at to dd/mm/yy format
   const date2 = new Date(edited_at);
   const day2 = date2.getUTCDate();
-  const month2 = date2.getUTCMonth() + 1; 
+  const month2 = date2.getUTCMonth() + 1;
   const year2 = date2.getUTCFullYear().toString().substr(-2);
   const edited_at_format = `${day2}/${month2}/${year2}`;
 
@@ -59,19 +59,21 @@ const Roadmap = ({ owner_id, creator_id, owner_name, creator_name, rid, views_co
               </div>
             </div>
             <div className='flex flex-col space-y-1 m-1'>
-              <div className='flex flex-row justify-between items-center overflow-hidden'>
-                <div className='text-lg font-bold truncate hover:truncate'>
+              {/**/}
+              <div className=' flex flex-row justify-between items-center'>
+                <div className=' text-md font-bold truncate w-[65%]'>
                   {title}
                 </div>
-                <div className='flex flex-row gap-4'>
-                  <div className='text-xs flex flex-wrap items-center gap-1'>
-                    <span><ShareIcon /></span> : {forks_count}
+                <div className='z-10 flex flex-row justify-center space-x-4 text-xs w-[35%]'>
+                  <div className='z-10 flex flex-row items-center '>
+                    <ShareIcon /> : {forks_count}
                   </div>
-                  <div className='text-xs flex flex-wrap items-center gap-1'>
-                    <span><LikeIcon /></span>: {stars_count}
+                  <div className='z-10 flex flex-row items-center '>
+                    <LikeIcon /> : {stars_count}
                   </div>
                 </div>
               </div>
+              {/**/}
               <div className='flex flex-row justify-between items-center'>
                 <div className='flex flex-row'>
                   <div className='text-sm'>
