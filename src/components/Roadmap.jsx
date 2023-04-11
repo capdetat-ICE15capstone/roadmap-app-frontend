069fc54dbf865ef24c18e36f56a8555ded37e4b1
wuttikorn.n45 <wuttikorn.n45@gmail.com>
2023-04-12 00:24:10 +0700

    adjust all data in roadmap component

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -36,41 +36,51 @@ const Roadmap = ({ owner_id, creator_id, owner_name, creator_name, rid, views_co
       <div className='flex flex-col bg-white rounded-3xl shadow-md w-[200px] h-[220px] mr-16 my-8 p-2 space-y-1'>
         <Link to={'/view/:id'} className='relative'>
           <img src={placeholderImage} className="rounded-2xl h-full w-full" />
-          <div className='absolute bottom-[5%] left-[5%] text-xs'>
-            test
+          <div className='absolute bottom-[5%] left-[5%] text-xs font-bold'>
+            {creator_name}
           </div>
-          <div className='absolute bottom-[5%] right-[5%] text-xs'>
-            test
+          <div className='absolute bottom-[5%] right-[5%] text-xs text-gray-600'>
+            Last updated: {edited_at}
           </div>
         </Link>
         <div className='flex flex-col space-y-1 m-1'>
           <div className='flex flex-row justify-between'>
-            <div className='text-xs font-bold'>
-              Roadmap...
+            <div className='text-lg font-bold'>
+              {title}
             </div>
-            <div className='text-xs'>
-              Fork: 69
-            </div>
-            <div className='text-xs'>
-              Star: 420
+            <div className='flex flex-row gap-4'> 
+              <div className='text-xs flex flex-wrap items-center gap-1'>
+                <span><ShareIcon/></span> : {forks_count}
+              </div>
+              <div className='text-xs flex flex-wrap items-center gap-1'>
+                <span><LikeIcon/></span>: {stars_count}
+              </div>
             </div>
           </div>
-          <div className='flex flex-row justify-between'>
-            <div className='text-xs'>
-              Owner: Amigo
+          <div className='flex flex-row justify-between items-center'>
+            <div className='flex flex-row'>
+              <div className='text-sm'>
+                Owner :
+              </div>
+              <button className='z-10 text-sm hover:text-blue-600 ml-1 ' onClick={handleClick}>
+                {owner_name}
+              </button>
             </div>
             <div className='text-xs text-gray-500'>
-              Created: 1/1/1
+              Created : {created_at}
             </div>
           </div>
-          <div className='flex flex-row'>
-            <div className='text-xs'>
-              Views: 69420
+          <div className='flex flex-row items-center'>
+            <span>
+              <EyeIcon className="flex stroke-1 stroke-gray-700 w-3/4 h-3/4 "/>
+            </span>
+            <div className='text-xs text-gray-700'>
+            : {views_count} views
             </div>
           </div>
         </div>
         {isOwner &&
-          <div className='relative flex left-[165px] -top-[20px]'>
+          <div className='relative flex left-[165px] -top-[25px]'>
           <RoadmapDropdown onDelete={deleteFunction}/>
         </div>}
       </div>
