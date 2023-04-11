d03054160a78d8036bc5dabf1ba0b24a249c8982
James <49901378+Mifuu@users.noreply.github.com>
2023-04-12 01:29:19 +0700

    Merge Setting (#18)
    * WIP
    
    Added SettingTab.jsx
    
    * WIP change tab working
    
    -Use parse function
    
    * Setting Profile and Setting Tab Update
    
    - Logic done
    - Set useState hook on submit done
    - Styling done (fingers crossed)
    - Setting Tab style done (without proper svg)
    - Setting Tab current tab indication done
    
    * Setting Account WIP
    
    * Small fixes
    
    * Setting Account Tab
    
    * Start PWA and noti config
    
    - add sw.js
    - add VitePWA to the project
    - start config of VitePWA in vite config file
    - start coding function for subscribing to noti server
    
    * add icon, change name and config manifest file
    
    * work on Notification and PWA
    
    - add manifest file for PWA
    - add push event listener for notification
    - add logic for making sub obj
    - add logic for sending sub obj to server
    - add logic for unsubscribing push service from push manager
    
    * To scroll
    
    * change service worker
    
    * change vapid key
    
    * Change setting icon
    
    * Add setting icons
    
    * Api WIP
    
    why is this so confusingly difficult
    
    * add noti sub unsub with back
    
    * API done?
    
    Add api for
    1. set public account
    2. change password
    3. deactivate account
    
    * Remove Profile Link and use Spinner
    
    * PWA Notification Done
    
    ---------
    
    Co-authored-by: SupakornSJB <limit4break@gmail.com>

---
+++ src/sw.js
@@ -0,0 +1,37 @@
+import { precacheAndRoute, cleanupOutdatedCaches } from "workbox-precaching";
+import { clientsClaim } from "workbox-core"
+
+cleanupOutdatedCaches()
+clientsClaim()
+
+const myCache = self.__WB_MANIFEST;
+precacheAndRoute(myCache);
+console.warn("Service worker is now operable");
+
+// listen for push notification
+self.addEventListener("push", (event) => {
+  // listen for push notification
+  const data = event.data.json();
+  console.log(data);
+  self.registration.showNotification(data.title, {
+    body: data.body,
+    icon: "android-chrome-192x192.png",
+  });
+});
+
+// use cache for offline event
+self.addEventListener("fetch", (event) => {
+  console.log(event);
+  if (!navigator.onLine) { // This is not good, if the server is down but internet online this would fail
+    console.log("offline");
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        if (response) return response;
+      })
+    );
+  }
+});
+
+self.addEventListener("fetch", (event) => {
+  
+})
