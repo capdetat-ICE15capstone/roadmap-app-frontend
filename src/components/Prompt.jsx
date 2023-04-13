7f5ce55c1a139706bbeb89b83498b92cdd23a36b
Hambaiki <92420824+Hambaiki@users.noreply.github.com>
2023-04-13 22:36:03 +0700

    Update prompt behavior to detect null title

--- src/components/Prompt.jsx
+++ src/components/Prompt.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-function Prompt({ title, message, positiveText, cancelText, positiveFunction, negativeFunction }) {
+function Prompt({ title, message, positiveText, negativeText, positiveFunction, negativeFunction }) {
 
   return (
     <div className="fixed inset-0 z-50">
       <div className="absolute inset-0 z-10 flex items-center justify-center">
-        <div className="flex flex-col rounded-xl bg-white shadow-lg">
+        <div className="flex flex-col rounded-xl bg-white shadow-lg max-w-md max-xs:max-w-xs">
           <div className="flex flex-col rounded-t-xl bg-gradient-to-b from-blue-900 to-main-blue">
-            <div className="text-xl text-white font-bold mx-4 my-2">
+            <div className={`${title ? 'visible' : 'hidden'} text-xl text-white font-bold mx-4 my-2`}>
               {title}
             </div>
           </div>
@@ -15,16 +15,16 @@ function Prompt({ title, message, positiveText, cancelText, positiveFunction, ne
             <div className='text-center'>
               {message}
             </div>
-            <div className={`${(cancelText && positiveText) && 'space-x-2'} flex justify-center`}>
+            <div className={`${(negativeText && positiveText) && 'space-x-2'} flex justify-center`}>
               <button
                 onClick={negativeFunction}
-                className={`${cancelText ? 'visible' : 'hidden'} ${positiveText && 'w-1/2'} bg-gray-500 text-white px-4 py-2 rounded-full text-sm font-bold transition ease-in-out hover:bg-gray-300 duration-300`}
+                className={`${negativeText ? 'visible' : 'hidden'} ${positiveText && 'w-1/2'} bg-gray-500 text-white px-4 py-2 rounded-full text-sm font-bold transition ease-in-out hover:bg-gray-700 duration-300`}
                 type="button">
-                {cancelText}
+                {negativeText}
               </button>
               <button
                 onClick={positiveFunction}
-                className={`${positiveText ? 'visible' : 'hidden'} ${cancelText && 'w-1/2'} bg-sub-blue text-white px-4 py-2 rounded-full text-sm font-bold transition ease-in-out hover:bg-mid-blue duration-300`}
+                className={`${positiveText ? 'visible' : 'hidden'} ${negativeText && 'w-1/2'} bg-sub-blue text-white px-4 py-2 rounded-full text-sm font-bold transition ease-in-out hover:bg-nav-blue duration-300`}
                 type="button"
               >
                 {positiveText}
