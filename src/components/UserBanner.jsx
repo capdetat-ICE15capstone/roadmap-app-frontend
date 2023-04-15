6db8e0860785d9a1a378368d688aac8d7658e0dc
Hambaiki <92420824+Hambaiki@users.noreply.github.com>
2023-04-15 16:44:41 +0700

    Improve responsiveness

--- src/components/UserBanner.jsx
+++ src/components/UserBanner.jsx
@@ -15,14 +15,14 @@ const UserBanner = ({ uid, username, exp, profile_picture_id }) => {
     const userLevel = Math.floor(exp / 100);
 
     return (
-        <Link to={`/home/${uid}`} className="flex justify-between items-center w-[75%] px-8 h-28 border-t-neutral-500 bg-slate-100 hover:bg-slate-200 shadow hover:transform hover:scale-[1.01] transition duration-100">
-            <div className='flex items-center w-[60%] space-x-4'>
-                <UserLogo />
+        <Link to={`/home/${uid}`} className="flex justify-between items-center w-[75%] px-4 h-28 border-t-neutral-500 bg-slate-100 hover:bg-slate-200 shadow hover:transform hover:scale-[1.01] transition duration-100">
+            <div className='flex items-center w-[60%] space-x-8'>
+                <UserLogo className='flex w-16 h-16 shrink-0'/>
                 <div className='flex items-center font-bold text-2xl w-2/5 max-w-sm'>
                     {username}
                 </div>
-                <div className="flex items-center justify-between w-2/5 max-w-sm px-6 py-4 text-sm font-bold text-white bg-blue-700 rounded-full">
-                    <div>
+                <div className="flex items-center justify-between w-2/5 max-w-sm px-2 py-4 text-sm font-bold text-white bg-blue-700 rounded-full">
+                    <div className='max-sm:hidden'>
                         LVL:
                     </div>
                     <div>
@@ -30,7 +30,7 @@ const UserBanner = ({ uid, username, exp, profile_picture_id }) => {
                     </div>
                 </div>
             </div>
-            <Triangle className="w-3 h-3 transform rotate-90 right-8 fill-current text-[#00286E]" />
+            <Triangle className="max-sm:hidden w-3 h-3 transform rotate-90 right-8 fill-current text-[#00286E]" />
         </Link>
 
 
