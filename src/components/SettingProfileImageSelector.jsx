c8629751a3d28bd38a15c008c0ea3a7400b067a1
James <49901378+Mifuu@users.noreply.github.com>
2023-04-15 22:02:15 +0700

    Responsive Setting + level requirement for profile picture

--- src/components/SettingProfileImageSelector.jsx
+++ src/components/SettingProfileImageSelector.jsx
@@ -5,7 +5,7 @@ import p1 from "../assets/profileImage_assets/placeholder1.png";
 import p2 from "../assets/profileImage_assets/placeholder2.png";
 import p3 from "../assets/profileImage_assets/placeholder3.jpeg";
 import p4 from "../assets/profileImage_assets/placeholder4.jpeg";
-import { update } from "lodash";
+import { ReactComponent as LockIcon } from "../assets/setting_assets/lock.svg";
 
 const lib = {
     0: p1,
@@ -38,7 +38,7 @@ export const getProfilePictureSrc = (id) => {
     return lib[0];
 }
 
-const SettingProfileImageSelector = ({isOpen, setIsOpen, selectedIndex = 0, setProfilePicture}) => {
+const SettingProfileImageSelector = ({isOpen, setIsOpen, selectedIndex = 0, setProfilePicture, level}) => {
     const [index, setIndex] = useState(0);
 
     useEffect (() => {
@@ -57,14 +57,26 @@ const SettingProfileImageSelector = ({isOpen, setIsOpen, selectedIndex = 0, setP
         setIsModalOpen(false);
     };
 
-    const ProfPic = ({id}) => {
+    const ProfPic = ({id, reqLevel}) => {
         return (
             //div className={`w-${side} h-${side} overflow-hidden rounded-full`}>
-            <button className={`px-2 py-2`} onClick={handleClickProfile(id)}>
-                <div className={`w-36 h-36 lg-w-40 lg-h-40 overflow-hidden rounded-full border-8 ${index == id ? "border-blue-800" : "border-transparent"}`}>
-                    <img src={getProfilePictureSrc(id)}
-                        className="w-36 lg-w-40 h-auto object-fit"
-                    />
+            <button className={`px-1 py-2`} onClick={handleClickProfile(id)} disabled={level < reqLevel}>
+                <div className="flex flex-col">
+                    <div className={`relative w-36 h-36 lg-w-40 lg-h-40 overflow-hidden rounded-full border-8 ${index == id ? "border-blue-800" : "border-transparent"}`}>
+                        {level < reqLevel ?
+                            <div className={`absolute w-full h-full overflow-hidden bg-black opacity-50`}>
+                                <div className="flex flex-col w-full h-full justify-center items-center">
+                                    <LockIcon width={64} height={64}/>
+                                </div>
+                            </div>
+                        : <></>}
+                        <div className="">
+                            <img src={getProfilePictureSrc(id)}
+                                className="w-36 lg-w-40 h-auto object-fit"
+                            />
+                        </div>
+                    </div>
+                    <p className={`${level < reqLevel? "text-black" : "text-transparent"}`}>{"Level: " + reqLevel}</p>
                 </div>
             </button>
         )
@@ -102,28 +114,28 @@ const SettingProfileImageSelector = ({isOpen, setIsOpen, selectedIndex = 0, setP
                             </h3>
                         </div>
                         {/* Images for select */}
-                        <div className="w-full mx-auto bg-gray-100 h-96 border-t border-b border-solid border-slate-200 overflow-y-scroll">
-                            <div className="flex flex-wrap px-5 gap-3 justify-evenly">
-                                <ProfPic id={0}/>
-                                <ProfPic id={1}/>
-                                <ProfPic id={2}/>
-                                <ProfPic id={3}/>
-                                <ProfPic id={4}/>
-                                <ProfPic id={5}/>
-                                <ProfPic id={6}/>
-                                <ProfPic id={7}/>
-                                <ProfPic id={8}/>
-                                <ProfPic id={9}/>
-                                <ProfPic id={10}/>
-                                <ProfPic id={11}/>
-                                <ProfPic id={12}/>
-                                <ProfPic id={13}/>
-                                <ProfPic id={14}/>
-                                <ProfPic id={15}/>
-                                <ProfPic id={16}/>
-                                <ProfPic id={17}/>
-                                <ProfPic id={18}/>
-                                <ProfPic id={19}/>
+                        <div className="w-full mx-auto bg-gray-100 h-[448px] border-t border-b border-solid border-slate-200 overflow-y-scroll">
+                            <div className="flex flex-wrap px-5 gap-1 justify-evenly my-2">
+                                <ProfPic id={0} reqLevel={0}/>
+                                <ProfPic id={1} reqLevel={0}/>
+                                <ProfPic id={2} reqLevel={0}/>
+                                <ProfPic id={3} reqLevel={0}/>
+                                <ProfPic id={4} reqLevel={0}/>
+                                <ProfPic id={5} reqLevel={3}/>
+                                <ProfPic id={6} reqLevel={6}/>
+                                <ProfPic id={7} reqLevel={9}/>
+                                <ProfPic id={8} reqLevel={12}/>
+                                <ProfPic id={9} reqLevel={15}/>
+                                <ProfPic id={10} reqLevel={18}/>
+                                <ProfPic id={11} reqLevel={21}/>
+                                <ProfPic id={12} reqLevel={24}/>
+                                <ProfPic id={13} reqLevel={27}/>
+                                <ProfPic id={14} reqLevel={30}/>
+                                <ProfPic id={15} reqLevel={33}/>
+                                <ProfPic id={16} reqLevel={36}/>
+                                <ProfPic id={17} reqLevel={39}/>
+                                <ProfPic id={18} reqLevel={42}/>
+                                <ProfPic id={19} reqLevel={45}/>
                             </div>
                         </div>
                         {/* Title */}
