53526ed58e968efa943abfe0ce0f402b535bdc40
Hambaiki <92420824+Hambaiki@users.noreply.github.com>
2023-04-20 15:35:58 +0700

    Feed logic restructure + animations!

--- src/functions/feedFunction.jsx
+++ src/functions/feedFunction.jsx
@@ -1,5 +1,27 @@
 import { axiosInstance } from '../functions/axiosInstance';
 
+// classify user input (roadmap(""), user("@""), tag("#""))
+export const classifyInput = (input) => {
+  // Search for USER case
+  if (input.charAt(0) === "@") {
+    const username = input.substring(1);
+    const type = "user"
+    console.log(`Searching for user ${username}`);
+    return type
+    // Search for TAG case  
+  } else if (input.charAt(0) === "#") {
+    const tag = input.substring(1);
+    const type = "tag"
+    console.log(`Searching for tag ${tag}`);
+    return type
+    // Search for ROADMAP case
+  } else {
+    const type = "roadmap"
+    console.log(`Searching for roadmap ${input}`);
+    return type
+  }
+}
+
 export async function getRid(searchValue) {
   const route = "/search/roadmap?title=" + searchValue;
   try {
