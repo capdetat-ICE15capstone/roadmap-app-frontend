cb4bf9436ae324caeb0cb6518ec10445adb4993a
Hambaiki <92420824+Hambaiki@users.noreply.github.com>
2023-04-29 14:01:32 +0700

    Improve login form responsiveness

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -71,6 +71,11 @@ export default function Login() {
   }
 
   async function submitForm(form) {
+    if (email === "" || password === "") {
+      setIsWarning(true);
+      setFailMessage("please input both email and password");
+      return;
+    }
     setIsLoading(true);
     const route = `/user/login/`;
     axiosInstance.post(route, form)
@@ -104,62 +109,69 @@ export default function Login() {
               duration: "0.3"
             }}
           >
-            <button type="button" onClick={() => navigate(-1)} className="absolute top-4 left-4 bg-black text-white shadow font-bold py-2 px-4 rounded-full transition ease-in-out hover:bg-mid-blue duration-300">
+            <button type="button" onClick={() => navigate('/intro')} className="absolute top-4 left-4 bg-black text-white shadow font-bold py-2 px-4 rounded-full transition ease-in-out hover:bg-mid-blue duration-300">
               Back
             </button>
-            <div className={`flex flex-row-reverse items-center justify-center h-4/5 max-h-80 w-4/5 max-w-lg rounded-2xl shadow-lg border border-gray-300`}>
-              <div className="flex flex-col p-6 h-full w-1/2 max-xs:w-full rounded-r-2xl max-xs:rounded-2xl justify-center items-center bg-white">
-                <div className="flex text-2xl font-black text-slate-600 mb-4">
-                  User Login
-                </div>
-                <form onSubmit={handleSubmit}>
-                  <div className="flex flex-col space-y-4 mb-4">
-                    <input
-                      type="email"
-                      className="shadow appearance-none border rounded-3xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                      id="InputEmail"
-                      placeholder="Email"
-                      onChange={(event) =>
-                        setEmail(event.target.value)
-                      }
-                    />
-                    <input
-                      type="password"
-                      className="shadow appearance-none border rounded-3xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                      id="InputPassword"
-                      placeholder="********"
-                      onChange={(event) =>
-                        setPassword(event.target.value)
-                      }
-                    />
-                  </div>
-                  <div className="flex flex-col mb-4">
-                    <div className="flex items-center justify-left">
-                      <label className='inline-flex'>
-                        <input
-                          type="checkbox"
-                          className="w-4 h-4 bg-gray-100 border-gray-300 rounded"
-                          id="CheckRememberMe"
-                          onChange={() => setRememberMe(!rememberMe)}
-                        />
-                      </label>
-                      <div className="ml-2 text-xs font-medium text-gray-400">
-                        Remember Me
-                      </div>
+            <div className={`flex flex-row-reverse items-center justify-center h-80 w-4/5 max-w-lg rounded-2xl sm:shadow-lg sm:border border-gray-300`}>
+              <div className="flex flex-col h-full w-1/2 max-sm:w-full rounded-r-2xl max-sm:rounded-2xl justify-center items-center sm:bg-white">
+                <div className='h-full w-full rounded-r-2xl'>
+                  <div className='flex flex-col justify-center items-center w-full h-full p-4'>
+                    <div className="flex text-2xl font-black text-slate-600 mb-4">
+                      User Login
                     </div>
+                    <form onSubmit={handleSubmit}>
+                      <div className="flex flex-col space-y-4 mb-4">
+                        <input
+                          type="email"
+                          className="shadow appearance-none border rounded-3xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                          id="InputEmail"
+                          placeholder="Email"
+                          onChange={(event) =>
+                            setEmail(event.target.value)
+                          }
+                        />
+                        <input
+                          type="password"
+                          className="shadow appearance-none border rounded-3xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                          id="InputPassword"
+                          placeholder="********"
+                          onChange={(event) =>
+                            setPassword(event.target.value)
+                          }
+                        />
+                      </div>
+                      <div className="flex flex-col mb-4">
+                        <div className="flex items-center justify-left">
+                          <label className='inline-flex'>
+                            <input
+                              type="checkbox"
+                              className="w-4 h-4 bg-gray-100 border-gray-300 rounded"
+                              id="CheckRememberMe"
+                              onChange={() => setRememberMe(!rememberMe)}
+                            />
+                          </label>
+                          <div className="ml-2 text-xs font-medium text-gray-400">
+                            Remember Me
+                          </div>
+                        </div>
+                      </div>
+                      <div className="flex flex-col space-y-2">
+                        <button type="submit" className="bg-sub-blue text-white shadow font-bold py-2 rounded-lg transition ease-in-out hover:bg-blue-900 duration-300">
+                          Log In
+                        </button>
+                        <button type="button" className="bg-transparent border border-sub-blue text-sub-blue shadow font-bold py-2 rounded-lg transition ease-in-out hover:bg-red-500 hover:border-transparent hover:text-white duration-300" onClick={() => navigate("/signup")}>
+                          Sign Up
+                        </button>
+                      </div>
+                    </form>
                   </div>
-                  <div className="flex flex-col space-y-2">
-                    <button type="submit" className="bg-sub-blue text-white shadow font-bold py-2 rounded-lg transition ease-in-out hover:bg-blue-900 duration-300">
-                      Log In
-                    </button>
-                    <button type="button" className="bg-transparent border border-sub-blue text-sub-blue shadow font-bold py-2 rounded-lg transition ease-in-out hover:bg-red-500 hover:border-transparent hover:text-white duration-300" onClick={() => navigate("/signup")}>
-                      Sign Up
-                    </button>
-                  </div>
-                </form>
+                </div>
+
               </div>
-              <div className="max-xs:hidden flex flex-col p-4 h-full w-1/2 rounded-l-2xl justify-center items-center bg-gradient-to-b from-teal-100 to-yellow-100">
-                <Logo className="w-4/5 h-4/5" />
+              <div className="max-sm:hidden flex h-full w-1/2 max-sm:w-full rounded-l-2xl justify-center items-center">
+                <div className='h-full w-full bg-gradient-to-b from-teal-100 to-yellow-100 rounded-l-2xl'>
+                  <Logo className="w-full h-full p-4" />
+                </div>
               </div>
             </div>
           </motion.div>
